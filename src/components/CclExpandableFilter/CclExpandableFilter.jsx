8f86d43a8d62404ab7c758f1c49661d430c6fb18
ionlizarazu <ilizarazu@codesyntax.com>
2021-03-31 17:02:29 +0200

    CclVerticalFilterMenu and CclExpandableFilter

---
+++ src/components/CclExpandableFilter/CclExpandableFilter.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const CclExpandableFilter = (props) => {
+  var {title, children} = props
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <>
+      <div className="ccl-dropdown__link  ccl-expandable__button" aria-expanded={expanded} onClick={() => setExpanded(!expanded)} >{title}</div>
+      <div className="ccl-form">
+        {children}
+      </div>
+    </>
+  );
+}
+
+export default CclExpandableFilter;
