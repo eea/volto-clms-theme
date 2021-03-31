05742c05e8882d71fc16a3c0b92a6dfa2011ea56
ionlizarazu <ilizarazu@codesyntax.com>
2021-03-31 17:01:54 +0200

    CclCard component

---
+++ src/components/CclCard/CclCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+
+import "./cards.less"
+
+import { defineMessages, useIntl } from 'react-intl';
+const messages = defineMessages({
+  accessToProduct: {
+    id: 'Access to product',
+    defaultMessage: 'Access to product',
+  },
+});
+
+function CclCard(props) {
+  var {type, card, children} = props
+  const intl = useIntl();
+
+  return (
+      <div className={"card-" + type}>
+        <div className="card-image">
+          {card.image.src && <img src={card.image.src} alt={card.image.alt} /> }
+        </div>
+        <div className="card-text">
+          <div className="card-title">
+            <a href="./dataset-catalogue/dataset-info.html">{card.title}</a>
+          </div>
+          <div className="card-description">
+            {card.description}
+          </div>
+          {type == 'block' &&
+            <div class="card-button">
+              <Link
+                to={card.absolute_url}
+                className="ccl-button ccl-button--default"
+              >
+                {intl.formatMessage(messages.accessToProduct)}
+              </Link>
+            </div>
+          }
+          {children}
+        </div>
+      </div>
+  )
+}
+
+export default CclCard
