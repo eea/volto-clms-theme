c115033caff32651d87d9d834573157008506c61
ionlizarazu <ilizarazu@codesyntax.com>
2021-04-16 14:43:26 +0200

    CclCardContainerBlock initial commit

---
+++ src/components/Blocks/CclCardContainerBlock/CclCardContainerBlockEdit.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import { SidebarPortal, BlocksToolbar, Icon, BlocksForm } from '@plone/volto/components'; // BlocksForm, Icon,
+import { CardContainerSchema } from './CardContainerSchema';
+import InlineForm from '@plone/volto/components/manage/Form/InlineForm';
+
+import { setSidebarTab } from '@plone/volto/actions';
+import CclCard from '@eea/volto-clms-theme/components/CclCard/CclCard'; 
+
+import "./styles.less"
+/*
+import { Button } from 'semantic-ui-react';
+import { isEmpty, without } from 'lodash';
+import {
+  emptyBlocksForm,
+  getBlocksLayoutFieldname,
+} from '@plone/volto/helpers';
+import {
+    getCards,
+  defaultNewCard,
+} from './utils';
+
+import tuneSVG from '@plone/volto/icons/column.svg';
+*/
+
+const CclCardContainerBlockEdit = props => {
+
+     const {
+            block,
+            data,
+            onChangeBlock,
+            pathname,
+            selected,
+            manage,
+        } = props;
+    // console.log(props)
+    const card = {
+      "title": "Dataset preview title",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus mauris ante, a iaculis leo placerat quis. Nullam vitae vulputate leo, et ultricies dolor.",
+      "image": {
+        "src": "https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg",
+        "alt": "Image alt text",
+      },
+      "absolute_url": "/en/product-portfolio/how-our-products-are-created",
+    }
+    
+    return (
+        <>
+            <div 
+                className="cardContainer-header"
+                onClick={() => {
+                    props.setSidebarTab(1);
+                  }}
+                  aria-hidden="true">
+                  {data.title || 'Card container'}
+            </div>
+                {data.customCards ? ('Add a custom card') : (
+                    <div className="card-container">
+                        <CclCard type={data.cardStyle || "line"} card={card} />
+                        <CclCard type={data.cardStyle || "line"} card={card} />
+                    </div>
+                    ) }
+                <SidebarPortal selected={selected}>
+                    <InlineForm
+                    schema={CardContainerSchema()}
+                    title="Card container block"
+                    onChangeField={(id, value) => {
+                      onChangeBlock(block, {
+                        ...data,
+                        [id]: value,
+                      });
+                    }}
+                    formData={data}
+                  />
+                </SidebarPortal>
+        </>
+
+        )
+}   
+
+
+export default CclCardContainerBlockEdit;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import CclContextNavigationBlockView from '@eea/volto-clms-theme/components/Bloc
 import CclContextNavigationBlockEdit from '@eea/volto-clms-theme/components/Blocks/CclContextNavigationBlock/CclContextNavigationBlockEdit';
 import navSVG from '@plone/volto/icons/nav.svg';
 
+import CclCardContainerBlockView from '@eea/volto-clms-theme/components/Blocks/CclCardContainerBlock/CclCardContainerBlockView';
+import CclCardContainerBlockEdit from '@eea/volto-clms-theme/components/Blocks/CclCardContainerBlock/CclCardContainerBlockEdit';
+import containerSVG from '@plone/volto/icons/apps.svg';
+
 import Schema from '@plone/volto/components/manage/Blocks/Block/Schema';
 
 const customBlocks = {
@@ -42,6 +46,42 @@ const customBlocks = {
       }
     }
   },
+
+  cardContainer: {
+    id: 'cardContainer', // The name (id) of the block
+    title: 'Card Container', // The display name of the block
+    icon: containerSVG, // The icon used in the block chooser
+    group: 'common', // The group (blocks can be grouped, displayed in the chooser)
+    view: CclCardContainerBlockView, // The view mode component
+    edit: CclCardContainerBlockEdit, // The edit mode component
+    restricted: false, // If the block is restricted, it won't show in the chooser
+    mostUsed: true, // A meta group `most used`, appearing at the top of the chooser
+    blockHasOwnFocusManagement: false, // Set this to true if the block manages its own focus
+    sidebarTab: 1, // The sidebar tab you want to be selected when selecting the block
+    security: {
+      addPermission: [], // Future proof (not implemented yet) add user permission role(s)
+      view: [], // Future proof (not implemented yet) view user role(s)
+    },
+    blockHasValue: (data) => {
+      // Returns true if the provided block data represents a value for the current block.
+      // Required for alternate default block types implementations.
+      // See also [Settings reference](/configuration/settings-reference)
+    },
+    // A block can have an schema enhancer function with the signature: (schema) => schema
+    // It can be either be at block level (it's applied always) or at a variation level
+    // The variation level one takes precedence.
+    schemaEnhancer: Schema,
+    // A block can define variations (it should include the stock, default one)
+    variations: {
+      default: { label: 'Default' },
+      custom: {
+        label: 'Custom',
+        // The variation level schema Enhancer function
+        schemaEnhancer: Schema
+      }
+    }
+  },
+
 };
 
 
@@ -57,6 +97,7 @@ const applyConfig = (config) => {
         ...config.blocks,
         blocksConfig: { ...config.blocks.blocksConfig, ...customBlocks },
     }
+
   return config;
 };
 
