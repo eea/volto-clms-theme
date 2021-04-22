85a35813e99cf58b1ee10ddec875d32feed936ce
Aitzol Naberan Burgaña <anaberan@codesyntax.com>
2021-04-22 14:22:10 +0200

    Use eslint 6.8.0 syntax as is installed in ci image

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es6": true
     },
     "extends": "plugin:react/recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 12,
+        "ecmaVersion": 2018,
         "sourceType": "module"
     },
     "plugins": [
