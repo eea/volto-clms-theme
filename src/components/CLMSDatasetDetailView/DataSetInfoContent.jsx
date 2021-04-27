2ad07dafab69aa76a53be439bad4a59012eff57e
bipoza <bipoza@gmail.com>
2021-04-27 15:28:55 +0200

    Basic CclInfoContainer

--- src/components/CLMSDatasetDetailView/DataSetInfoContent.jsx
+++ src/components/CLMSDatasetDetailView/DataSetInfoContent.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-
+import { CclInfoDescription, CclInfoContainer } from '../CclInfoDescription';
 const DataSetInfoContent = (data) => {
-  return <div>DataSetInfoContent component!</div>;
+  return (
+    <div>
+      <CclInfoContainer>
+        <h2>Container title</h2>
+        <CclInfoDescription
+          title="Kaixo"
+          description="Kaixo deskribapena"
+        ></CclInfoDescription>
+      </CclInfoContainer>
+    </div>
+  );
 };
 
 export default DataSetInfoContent;
