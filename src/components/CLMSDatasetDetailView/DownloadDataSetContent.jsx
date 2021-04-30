6e05ae644a570bf77d608092af2e1deb57e44dd0
Unai <uetxaburu@codesyntax.eus>
2021-04-30 09:59:02 +0200

    import React from 'react'; import { CclInfoDescription, CclInfoContainer } from '../CclInfoDescription'; import CclCitation from '@eeacms/volto-clms-theme/components/CclCitation/CclCitation'; import CclButton from '@eeacms/volto-clms-theme/components/CclButton/CclButton'; import { StringToHTML } from '@eeacms/volto-clms-theme/components/CclUtils'; import CclCard from '@eeacms/volto-clms-theme/components/CclCard/CclCard'; import CclCardContainerBlockView from '@eeacms/volto-clms-theme/components/Blocks/CclCardContainerBlock/CclCardContainerBlockView';
    const DataSetInfoContent = (data) => {
      return (
        <div>
          <CclCitation title="Validation status (MOCK)" marginBottom={true}>
            <p>
              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus
              mauris ante, a iaculis leo placerat quis.
            </p>
          </CclCitation>
          <CclInfoContainer>
            <h2>Dataset Info</h2>
    
            {data?.dataResourceAbstract?.data && (
              <CclInfoDescription
                title="Data resource title"
                description={data.dataResourceTitle}
              ></CclInfoDescription>
            )}
    
            {data?.dataResourceAbstract?.data && (
              <CclInfoDescription
                title="Data resource abstract"
                description={
                  <StringToHTML string={data.dataResourceAbstract.data} />
                }
                tooltip="Hello word!"
              ></CclInfoDescription>
            )}
    
            {data?.dataSources?.data && (
              <CclInfoDescription
                title="Resource type"
                description={<StringToHTML string={data.dataSources.data} />}
                tooltip="Hello word!"
              ></CclInfoDescription>
            )}
            {data?.dataResourceLocator && (
              <>
                <CclInfoDescription
                  title="Resource locator"
                  description={
                    <>
                      {data.dataResourceLocator}
                      <br />
                      <CclButton url={data.dataResourceLocator} target="_blank">
                        Go to resource locator
                      </CclButton>
                    </>
                  }
                ></CclInfoDescription>
              </>
            )}
            <div class="dataset-info-documents dropdown">
              <div class="ccl-expandable__button" aria-expanded="true">
                <h2>Technical documents (X docs)</h2>
              </div>
              <div class="documents-dropdown">
                <CclCard
                  title={data.title}
                  description={data.description}
                  docInfo={data.docInfo}
                ></CclCard>
                <CclCard
                  title={data.title}
                  description={data.description}
                  docInfo={data.docInfo}
                ></CclCard>
              </div>
            </div>
            <h2>Found the dataset in this products</h2>
            <div class="card-container">
              <CclCard
              type="block"
              title="Dataset title"
              description={data.description}
              image={data.image}
            ></CclCard><CclCard
            type="block"
            title="Dataset title"
            description={data.description}
            image={data.image}
          ></CclCard>
            </div>
          </CclInfoContainer>
        </div>
      );
    };
    
    export default DataSetInfoContent;

---
+++ src/components/CLMSDatasetDetailView/DownloadDataSetContent.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import CclTable from '@eeacms/volto-clms-theme/components/CclTable/CclTable';
+import CclButton from '@eeacms/volto-clms-theme/components/CclButton/CclButton';
+import { Link } from 'react-router-dom';
+
+const DownloadDataSetContent = (data, type) => {
+  let url = '/register';
+
+  return (<div>
+            <div className="login-block">
+              <div className="login-content">
+                <CclButton url={"/login"}>Login to download the data</CclButton>
+                <p className="login-block-new">
+                  New user?{' '}
+                  <Link to={url}>{'Follow this link to register'}</Link>
+                </p>
+              </div>
+            </div>
+            <div className="dataset-download-area">
+              <h2>Download by area</h2>
+              <p>
+                Use this option if you would like to download the dataset for
+                area(s) of interest.
+              </p>
+              <CclButton disabled={true}>Go to download by area</CclButton>
+            </div>
+            <CclTable
+            type={type} dataset={data}
+            ></CclTable>
+          </div>
+  );
+};
+
+export default DownloadDataSetContent;
