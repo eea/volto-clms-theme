0c584d249b5e7805d96618d6ed3e0a14a4a9c6f0
Unai <uetxaburu@codesyntax.eus>
2021-05-06 09:26:04 +0200

    footer dynamic menu

---
+++ src/components/CclTopMainMenu/CclFooterMenu.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { getItemsByPath } from '@collective/volto-dropdownmenu/utils';
+import { Link } from 'react-router-dom';
+
+const CclFooterMenu = () => {
+  const dropdownMenuNavItems = useSelector(
+    (state) => state.dropdownMenuNavItems?.result,
+  );
+  const pathname = useSelector((state) => state.router?.location.pathname);
+  const menu = getItemsByPath(dropdownMenuNavItems, pathname);
+
+  return (
+    <>
+      {menu?.length > 0
+        ? menu
+            ?.filter((item) => item.visible)
+            ?.map((item) => (
+              <li>
+                <Link to={{ pathname: item.linkUrl?.[0]?.['@id'] }}>
+                  {item.title}
+                </Link>
+              </li>
+            ))
+        : null}
+    </>
+  );
+};
+
+export default CclFooterMenu;
