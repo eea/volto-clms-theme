c671a491d5d2a525c35b3b1b0a37463fa8a45633
Unai <uetxaburu@codesyntax.eus>
2021-05-17 10:07:23 +0200

    event view

--- src/components/CLMSEvents/EventsView.jsx
+++ src/components/CLMSEvents/EventsView.jsx
@@ -1,17 +1,5 @@
-/**
- * Full view component.
- * @module components/theme/View/EventsView
- */
-
 import React from 'react';
-import PropTypes from 'prop-types';
 import CclCard from '@eeacms/volto-clms-theme/components/CclCard/CclCard';
-/**
- * Full view component class.
- * @function EventsView
- * @param {Object} content Content object.
- * @returns {string} Markup of the component.
- */
 
 const EventsView = (content) => {
   return (
@@ -42,35 +30,6 @@ const EventsView = (content) => {
       </div>
     </>
   );
-};
-
-/**
- * Property types.
- * @property {Object} propTypes Property types.
- * @static
- */
-EventsView.propTypes = {
-  /**
-   * Content of the object
-   */
-  content: PropTypes.shape({
-    /**
-     * Title of the object
-     */
-    title: PropTypes.string,
-    /**
-     * Description of the object
-     */
-    description: PropTypes.string,
-    /**
-     * Date of the object
-     */
-    when: PropTypes.string,
-    /**
-     * Place of the object
-     */
-    where: PropTypes.string,
-  }),
 };
 
 export default EventsView;
