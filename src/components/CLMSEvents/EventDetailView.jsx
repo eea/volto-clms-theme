7f6de56bc2e58e16426ada46fc5db304582d638c
Unai <uetxaburu@codesyntax.eus>
2021-05-17 10:10:27 +0200

    event view with prettier

--- src/components/CLMSEvents/EventDetailView.jsx
+++ src/components/CLMSEvents/EventDetailView.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { StringToHTML } from '@eeacms/volto-clms-theme/components/CclUtils';
 
-
 export const EventDetailView = (content) => {
   return (
-      <div className="ccl-container">
-        <div className="event-detail">
-          <img
-            src="https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg"
-            alt="Placeholder"
-          ></img>
-          <div className="event-detail-content">
+    <div className="ccl-container">
+      <div className="event-detail">
+        <img
+          src="https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg"
+          alt="Placeholder"
+        ></img>
+        <div className="event-detail-content">
           <StringToHTML string={content.text?.data || ''} />
-          </div>
         </div>
       </div>
+    </div>
   );
 };
 
