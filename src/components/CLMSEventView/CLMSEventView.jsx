079381219b14aa388aa4a6df8b2e56fe22ff6160
ionlizarazu <ilizarazu@codesyntax.com>
2021-05-17 15:58:09 +0200

    rename components

--- src/components/CLMSEventView/CLMSEventView.jsx
+++ src/components/CLMSEventView/CLMSEventView.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { StringToHTML } from '@eeacms/volto-clms-theme/components/CclUtils';
 
-export const EventDetailView = (content) => {
+export const CLMSEventView = (props) => {
+  const { content } = props;
   return (
     <div className="ccl-container">
+      <h1 className="page-title">{content.title}</h1>
       <div className="event-detail">
         <img
-          src="https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg"
+          src={
+            content?.image
+              ? content?.image?.download
+              : 'https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg'
+          }
           alt="Placeholder"
         ></img>
         <div className="event-detail-content">
@@ -17,4 +23,4 @@ export const EventDetailView = (content) => {
   );
 };
 
-export default EventDetailView;
+export default CLMSEventView;
