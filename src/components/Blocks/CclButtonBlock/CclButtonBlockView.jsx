592ba33d259c5bbeae79df28cb7de92bf960820a
Unai <uetxaburu@codesyntax.eus>
2021-10-27 12:26:03 +0200

    Some fixes after Bilbomatica tests

--- src/components/Blocks/CclButtonBlock/CclButtonBlockView.jsx
+++ src/components/Blocks/CclButtonBlock/CclButtonBlockView.jsx
@@ -5,25 +5,45 @@ import { flattenToAppURL } from '@plone/volto/helpers/Url/Url';
 const CclButtonBlockView = (props) => {
   const { data } = props;
   const flattern_url = flattenToAppURL(data?.href?.[0]?.['@id']);
-
-  return (
-    <CclButton
-      url={
-        data.download && data?.href?.[0]?.['@type'] === 'File'
-          ? flattern_url + '/@@download/file'
-          : flattern_url
-      }
-      disabled={data?.disabled}
-      download={data?.download || data?.href?.[0]?.['@type'] === 'File'}
-      target={
-        data.target ||
-        (data.download && data.href[0]['@type'] === 'File' && '_blank')
-      }
-      mode={data.style}
-    >
-      {data.title || 'Text example...'}
-    </CclButton>
-  );
+  return {
+    ...(data.style === 'left menu' ? (
+      <div className="left-menu-detail">
+        <CclButton
+          url={
+            data.download && data?.href?.[0]?.['@type'] === 'File'
+              ? flattern_url + '/@@download/file'
+              : flattern_url
+          }
+          disabled={data?.disabled}
+          download={data?.download || data?.href?.[0]?.['@type'] === 'File'}
+          target={
+            data.target ||
+            (data.download && data.href[0]['@type'] === 'File' && '_blank')
+          }
+          mode={data.style}
+        >
+          {data.title || 'Text example...'}
+        </CclButton>
+      </div>
+    ) : (
+      <CclButton
+        url={
+          data.download && data?.href?.[0]?.['@type'] === 'File'
+            ? flattern_url + '/@@download/file'
+            : flattern_url
+        }
+        disabled={data?.disabled}
+        download={data?.download || data?.href?.[0]?.['@type'] === 'File'}
+        target={
+          data.target ||
+          (data.download && data.href[0]['@type'] === 'File' && '_blank')
+        }
+        mode={data.style}
+      >
+        {data.title || 'Text example...'}
+      </CclButton>
+    )),
+  };
 };
 
 export default CclButtonBlockView;
