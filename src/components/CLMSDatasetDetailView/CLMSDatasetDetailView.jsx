dc941a3fedcf29760274d15c0d1c0c2f0a919c75
ionlizarazu <ilizarazu@codesyntax.com>
2021-12-10 12:15:11 +0100

    add a segment to import data from geonetwork endpoints

--- src/components/CLMSDatasetDetailView/CLMSDatasetDetailView.jsx
+++ src/components/CLMSDatasetDetailView/CLMSDatasetDetailView.jsx
@@ -18,6 +18,11 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import jwtDecode from 'jwt-decode';
 import { injectIntl } from 'react-intl';
+import { postImportGeonetwork } from '../../actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { Segment, Modal } from 'semantic-ui-react';
+import { getUser } from '@plone/volto/actions';
+
 // import {
 //   mockDatabaseInfo,
 //   mockMetadata,
@@ -35,10 +40,126 @@ import { injectIntl } from 'react-intl';
 
 const CLMSDatasetDetailView = ({ content, token }) => {
   const location = useLocation();
+  const dispatch = useDispatch();
+  const geonetwork_importation = useSelector(
+    (state) => state.geonetwork_importation,
+  );
+  const userSession = useSelector((state) => state.userSession);
+  const user = useSelector((state) => state.users?.user);
+  const user_token = userSession.token ? jwtDecode(userSession.token).sub : '';
+  React.useEffect(() => {
+    dispatch(getUser(user_token));
+  }, [dispatch, user_token]);
+
+  function handleImport(id, type) {
+    dispatch(postImportGeonetwork(location.pathname, id, type));
+  }
+  const [open, setOpen] = React.useState({});
 
   return (
     <div className="ccl-container ">
       <h1 className="page-title">{content.title}</h1>
+      {content.geonetwork_identifiers?.items?.length > 0 &&
+        user.roles &&
+        user.roles.includes('Manager') && (
+          <Segment.Group compact horizontal>
+            {content.geonetwork_identifiers?.items.map((item) => {
+              return (
+                <Segment
+                  padded={'very'}
+                  color={'olive'}
+                  key={item.id}
+                  loading={geonetwork_importation.loading}
+                  circular
+                >
+                  <strong>
+                    {item.title} (from {item.type}):{' '}
+                  </strong>
+                  <br />
+                  <br />
+                  <Modal
+                    onClose={() => {
+                      setOpen({ ...open, [item.id]: false });
+                    }}
+                    onOpen={() => {
+                      setOpen({ ...open, [item.id]: true });
+                    }}
+                    open={open[item.id]}
+                    trigger={<CclButton>Import data</CclButton>}
+                    className={'modal-clms'}
+                    size={'fullscreen'}
+                  >
+                    <div className={'modal-clms-background'}>
+                      <div className={'modal-clms-container'}>
+                        <div className={'modal-close modal-clms-close'}>
+                          <span
+                            className="ccl-icon-close"
+                            aria-label="Close"
+                            onClick={() => {
+                              setOpen({ ...open, [item.id]: false });
+                            }}
+                            onKeyDown={() => {
+                              setOpen({ ...open, [item.id]: false });
+                            }}
+                            tabIndex="0"
+                            role="button"
+                          ></span>
+                        </div>
+                        <div className="modal-login-text">
+                          <h1>Import from GeoNetwork</h1>
+                          This action will import the data from{' '}
+                          <strong>{item.title}</strong> (from {item.type}) into
+                          this dataset.
+                          <br />
+                          <br />
+                          <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href={
+                              'https://sdi.eea.europa.eu/catalogue/copernicus/api/records/' +
+                              item.id
+                            }
+                          >
+                            Geonetwork element
+                          </a>
+                        </div>
+                        <CclButton
+                          onClick={() => {
+                            handleImport(item.id, item.type);
+                            setOpen({ ...open, [item.id]: false });
+                          }}
+                          mode="filled"
+                        >
+                          Import data
+                        </CclButton>
+                      </div>
+                    </div>
+                  </Modal>
+                  {geonetwork_importation.imported_data
+                    ?.requested_geonetwork_id === item.id && (
+                    <p>
+                      {geonetwork_importation.loaded &&
+                        geonetwork_importation.error === null && (
+                          <strong>
+                            {' '}
+                            The data has been successfully imported
+                          </strong>
+                        )}
+                    </p>
+                  )}
+                  {geonetwork_importation.imported_data?.status === 'error' && (
+                    <p>
+                      <strong>
+                        {' '}
+                        {geonetwork_importation.imported_data?.message}
+                      </strong>
+                    </p>
+                  )}
+                </Segment>
+              );
+            })}
+          </Segment.Group>
+        )}
       <CclTabs>
         <div tabTitle="General Info">{DataSetInfoContent(content)}</div>
         <div tabTitle="Metadata">{MetadataContent(content)}</div>
