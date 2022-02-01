73a4a252cba51e338e9c79572f92abc5c5a6ea16
ionlizarazu <ilizarazu@codesyntax.com>
2022-02-01 13:10:55 +0100

    subjects to taxonomy_technical_library_categorization at TL view

--- src/components/CLMSDownloadableFileView/CLMSDownloadableFileView.jsx
+++ src/components/CLMSDownloadableFileView/CLMSDownloadableFileView.jsx
@@ -19,19 +19,20 @@ export const CLMSDownloadableFileView = (props) => {
             <p>{content.description}</p>
           </div>
         </div>
-        {content?.subjects && content?.subjects?.length > 0 && (
-          <Label.Group>
-            {content?.taxonomy_technical_library_categorization?.map(
-              (keyword, key) => {
-                return (
-                  <Label key={key} color="olive">
-                    {keyword.title}
-                  </Label>
-                );
-              },
-            )}
-          </Label.Group>
-        )}
+        {content?.taxonomy_technical_library_categorization &&
+          content?.taxonomy_technical_library_categorization?.length > 0 && (
+            <Label.Group>
+              {content?.taxonomy_technical_library_categorization?.map(
+                (keyword, key) => {
+                  return (
+                    <Label key={key} color="olive">
+                      {keyword.title}
+                    </Label>
+                  );
+                },
+              )}
+            </Label.Group>
+          )}
         <CclButton download={true} url={content?.file?.download}>
           Download file
         </CclButton>
