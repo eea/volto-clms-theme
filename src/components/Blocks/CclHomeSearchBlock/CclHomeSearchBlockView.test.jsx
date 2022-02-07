52751d4237c26e9319e1c5d75fa2b39d7d05cee3
Unai <uetxaburu@codesyntax.eus>
2022-02-07 17:00:53 +0100

    test up to 59%

--- src/components/Blocks/CclHomeSearchBlock/CclHomeSearchBlockView.test.jsx
+++ src/components/Blocks/CclHomeSearchBlock/CclHomeSearchBlockView.test.jsx
@@ -56,4 +56,58 @@ describe('CclSearchBlock', () => {
       .toJSON();
     expect(SearchBlockEdit).toMatchSnapshot();
   });
+  it('Check search block edit with http/https url', () => {
+    const mockSearchBlockPros2 = {
+      link: [{ '@id': 'https://google.com' }],
+      linkText: 'Google',
+    };
+    const SearchBlockEdit = renderer
+      .create(
+        <MemoryRouter>
+          <IntlProvider locale="en">
+            <CclSearchBlockEdit data={mockSearchBlockPros2}>
+              <p>Search block edit test</p>
+            </CclSearchBlockEdit>
+          </IntlProvider>
+        </MemoryRouter>,
+      )
+      .toJSON();
+    expect(SearchBlockEdit).toMatchSnapshot();
+  });
+
+  it('Check search block edit with http/https url without link text', () => {
+    const mockSearchBlockPros2 = {
+      link: [{ '@id': 'https://google.com' }],
+    };
+    const SearchBlockEdit = renderer
+      .create(
+        <MemoryRouter>
+          <IntlProvider locale="en">
+            <CclSearchBlockEdit data={mockSearchBlockPros2}>
+              <p>Search block edit test</p>
+            </CclSearchBlockEdit>
+          </IntlProvider>
+        </MemoryRouter>,
+      )
+      .toJSON();
+    expect(SearchBlockEdit).toMatchSnapshot();
+  });
+  it('Check search block view with http/https url', () => {
+    const data = {
+      link: [{ '@id': 'https://google.com' }],
+      linkText: 'Google',
+    };
+    const SearchBlockView = renderer
+      .create(
+        <MemoryRouter>
+          <IntlProvider locale="en">
+            <CclSearchBlockView data={data}>
+              <p>Search block view test</p>
+            </CclSearchBlockView>
+          </IntlProvider>
+        </MemoryRouter>,
+      )
+      .toJSON();
+    expect(SearchBlockView).toMatchSnapshot();
+  });
 });
