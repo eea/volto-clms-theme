16b86fd056cc2790335505f80fc67d37c674f260
ionlizarazu <ilizarazu@codesyntax.com>
2022-02-10 13:46:37 +0100

    downloadable files modifications

--- src/components/CclDownloadTable/CclDownloadTable.jsx
+++ src/components/CclDownloadTable/CclDownloadTable.jsx
@@ -1,10 +1,13 @@
 import './download-table.less';
 
+import { Checkbox, Input, Pagination } from 'semantic-ui-react';
 import React, { useState } from 'react';
 
 import CclButton from '@eeacms/volto-clms-theme/components/CclButton/CclButton';
-import { Checkbox } from 'semantic-ui-react';
+import { Icon } from '@plone/volto/components';
 import PropTypes from 'prop-types';
+import paginationLeftSVG from '@plone/volto/icons/left-key.svg';
+import paginationRightSVG from '@plone/volto/icons/right-key.svg';
 import useCartState from '@eeacms/volto-clms-utils/cart/useCartState';
 import { useSelector } from 'react-redux';
 
@@ -13,6 +16,13 @@ function CclDownloadTable(props) {
   const { dataset } = props;
   const { addCartItem, Toast, isLoggedIn } = useCartState();
   const [cartSelection, setCartSelection] = useState([]);
+  const [currentPageItems, setCurrentPageItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filterText, setFilterText] = useState('');
+  const [filteredItems, setFilteredItems] = useState('');
+  const [totalPages, setTotalPages] = useState(
+    Math.ceil(dataset?.downloadable_files?.items.length / 10),
+  );
   // complete the selected file with dataset UID, title and a concat of dataset.UID and block id to get unique id for the whole web
   const prePackagedCollection = dataset?.downloadable_files?.items.map(
     (element) => {
@@ -25,16 +35,60 @@ function CclDownloadTable(props) {
     },
   );
 
+  React.useEffect(() => {
+    setCurrentPageItems(prePackagedCollection.slice(0, 10));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  React.useEffect(() => {
+    let fItems = prePackagedCollection.filter((item) =>
+      item.resolution.toLowerCase().includes(filterText.toLocaleLowerCase()),
+    );
+    if (filterText) {
+      setCurrentPageItems(fItems.slice(0, 10));
+      setFilteredItems(fItems);
+      setTotalPages(Math.ceil(fItems.length / 10));
+    } else {
+      setCurrentPageItems(prePackagedCollection.slice(0, 10));
+      setFilteredItems(prePackagedCollection);
+      setTotalPages(Math.ceil(prePackagedCollection.length / 10));
+    }
+  }, [filterText, prePackagedCollection]);
+
+  const onPaginationChange = (e, { activePage }) => {
+    setCurrentPage(activePage);
+    if (filterText) {
+      setCurrentPageItems(
+        filteredItems.slice(activePage * 10 - 10, activePage * 10),
+      );
+    } else {
+      setCurrentPageItems(
+        prePackagedCollection.slice(activePage * 10 - 10, activePage * 10),
+      );
+    }
+  };
+
   const selectCart = (id, checked) => {
     if (checked) setCartSelection(cartSelection.concat(id));
     else setCartSelection(cartSelection.filter((arr_id) => arr_id !== id));
   };
 
   const selectAllCart = (checked) => {
+    let newCartSelection = [];
     if (checked) {
-      setCartSelection(prePackagedCollection.map((item) => item.unique_id));
+      newCartSelection = [
+        ...cartSelection,
+        ...currentPageItems.map((item) => item.unique_id),
+      ];
+      setCartSelection(newCartSelection);
     } else {
-      setCartSelection([]);
+      newCartSelection = [...cartSelection];
+      currentPageItems.forEach((pageItem) => {
+        newCartSelection = newCartSelection.filter(
+          (cartUid) => pageItem.unique_id !== cartUid,
+        );
+      });
+      setCartSelection(newCartSelection);
     }
   };
 
@@ -62,6 +116,16 @@ function CclDownloadTable(props) {
         contact us.
       </p>
       <p>{dataset?.description || 'Download table default description'}</p>
+      <div className="search-input">
+        <Input
+          id={`${props.id}-searchtext`}
+          placeholder={'Filter by Resolution'}
+          fluid
+          onChange={(event, { value }) => {
+            setFilterText(value);
+          }}
+        />
+      </div>
       <div className="custom-table dataset-table">
         <table>
           <thead>
@@ -71,8 +135,8 @@ function CclDownloadTable(props) {
                   <Checkbox
                     onChange={(e, data) => selectAllCart(data.checked)}
                     checked={
-                      prePackagedCollection
-                        ? prePackagedCollection
+                      currentPageItems
+                        ? currentPageItems
                             .map((item, key) => item.unique_id)
                             .every(function (val) {
                               return cartSelection.indexOf(val) !== -1;
@@ -91,8 +155,8 @@ function CclDownloadTable(props) {
             </tr>
           </thead>
           <tbody>
-            {prePackagedCollection
-              ? prePackagedCollection.map((dataset_file, key) => {
+            {currentPageItems
+              ? currentPageItems.map((dataset_file, key) => {
                   return (
                     <tr key={key}>
                       {isLoggedIn && (
@@ -128,6 +192,29 @@ function CclDownloadTable(props) {
               : false}
           </tbody>
         </table>
+        <div className="pagination-wrapper">
+          <Pagination
+            activePage={currentPage}
+            totalPages={totalPages}
+            onPageChange={(e, { activePage }) => {
+              onPaginationChange(e, { activePage });
+            }}
+            firstItem={null}
+            lastItem={null}
+            prevItem={{
+              content: <Icon name={paginationLeftSVG} size="18px" />,
+              icon: true,
+              'aria-disabled': currentPage === 1,
+              className: currentPage === 1 ? 'disabled' : null,
+            }}
+            nextItem={{
+              content: <Icon name={paginationRightSVG} size="18px" />,
+              icon: true,
+              'aria-disabled': currentPage === totalPages,
+              className: currentPage === totalPages ? 'disabled' : null,
+            }}
+          ></Pagination>
+        </div>
       </div>
 
       <CclButton
