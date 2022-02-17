669643a41d16c84c044b564047fee161a10a1e80
ionlizarazu <ilizarazu@codesyntax.com>
2022-02-17 15:30:49 +0100

    gemet inspire themes label renaming

--- src/components/Blocks/CustomTemplates/VoltoSearchBlock/rewriteOptions.js
+++ src/components/Blocks/CustomTemplates/VoltoSearchBlock/rewriteOptions.js
@@ -9,6 +9,28 @@ export const portal_types_labels = {
   TechnicalLibrary: 'Product documentation',
 };
 
+export const category_labels = {
+  farming: 'Farming',
+  biota: 'Biota',
+  boundaries: 'Boundaries',
+  climatologyMeteorologyAtmosphere: 'Climatology / Meteorology / Atmosphere',
+  economy: 'Economy',
+  elevation: 'Elevation',
+  environment: 'Environment',
+  geoscientificInformation: 'Geoscientific Information',
+  health: 'Health',
+  imageryBaseMapsEarthCover: 'Imagery / Base Maps / Earth Cover',
+  intelligenceMilitary: 'Intelligence / Military',
+  inlandWaters: 'InlandWaters',
+  location: 'Location',
+  oceans: 'Oceans',
+  planningCadastre: 'Planning Cadastre',
+  society: 'Society',
+  structure: 'Structure',
+  transportation: 'Transportation',
+  utilitiesCommunication: 'Utilities / Communication',
+};
+
 const rewriteOptions = (name, choices) => {
   var result = choices;
   if (name === 'review_state') {
@@ -27,12 +49,25 @@ const rewriteOptions = (name, choices) => {
     'Tender',
     'TechnicalLibrary',
   ];
-
   if (name === 'portal_type') {
     result = choices
       .filter((opt) => portal_types.includes(opt.value))
       .map((opt) => {
         return { ...opt, label: portal_types_labels[opt.value] };
+      })
+      .sort((a, b) => {
+        if (a.label < b.label) {
+          return -1;
+        } else if (a.label > b.label) {
+          return 1;
+        }
+        return 0;
+      });
+  }
+  if (name === 'classificationTopicCategory') {
+    result = choices
+      .map((opt) => {
+        return { ...opt, label: category_labels[opt.value] };
       })
       .sort((a, b) => {
         if (a.label < b.label) {
