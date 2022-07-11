30db876826dd53db6bbb11507de11af10a71a4eb
Unai <uetxaburu@codesyntax.eus>
2022-07-11 12:43:06 +0200

    subscription modal block

--- src/components/Blocks/CclSubscriptionBlock/SubscriptionView.jsx
+++ src/components/Blocks/CclSubscriptionBlock/SubscriptionView.jsx
@@ -1,9 +1,20 @@
 import { SubscriptionView } from '@eeacms/volto-clms-theme/components/CLMSSubscriptionView';
+import CclModal from '@eeacms/volto-clms-theme/components/CclModal/CclModal';
+import CclButton from '@eeacms/volto-clms-theme/components/CclButton/CclButton';
 
 const SubscriptionBlockView = (props) => {
   const { data } = props;
 
-  return <SubscriptionView type={data.type || 'events'} />;
+  return (
+    <CclModal
+      trigger={
+        <CclButton mode={'filled'}>{data.title || 'Text example...'}</CclButton>
+      }
+      size="small"
+    >
+      <SubscriptionView type={data.type || 'events'} />
+    </CclModal>
+  );
 };
 
 export default SubscriptionBlockView;
