a35faad04dc54e78afa9fc47fe9545ff7fe36303
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-07-14 08:40:31 +0200

    show related products/datasets always

--- src/components/CLMSNewsItemView/CLMSNewsItemView.jsx
+++ src/components/CLMSNewsItemView/CLMSNewsItemView.jsx
@@ -75,127 +75,115 @@ const CLMSNewsItemView = (props) => {
               <StringToHTML string={content.text?.data || ''} />
             </div>
             <LightGalleryListing />
-            {files.length > 0 && (
-              <CclListingCards
-                variation="file"
-                items={files}
-                linkHref={`${files['@id']}/@@download/file`}
-              />
-            )}
-            {content?.products?.length > 0 && (
-              <Accordion fluid styled>
-                <Accordion.Title
-                  as={'h2'}
-                  onClick={() => handleClick({ index: 0 })}
-                  className={'accordion-title align-arrow-right'}
-                >
-                  {activeIndex.includes(0) ? (
-                    <Icon
-                      name={iconName(content, titleIcons.opened)}
-                      size="24px"
-                    />
-                  ) : (
-                    <Icon
-                      name={iconName(content, titleIcons.closed)}
-                      size="24px"
-                    />
-                  )}
-                  <span>Related products</span>
-                </Accordion.Title>
-                <Accordion.Content active={activeIndex.includes(0)}>
-                  <AnimateHeight animateOpacity duration={500} height={'auto'}>
-                    <ul>
-                      {content.products.map((product, key) => (
-                        <li key={key}>
-                          <Grid columns={2}>
-                            <Grid.Column width={2}>
-                              {product.image_field && (
-                                <img
-                                  src={
-                                    product.image_field
-                                      ? `${product['@id']}/@@images/image`
-                                      : 'https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg'
-                                  }
-                                  alt={
-                                    product.image_field
-                                      ? product.image?.filename
-                                      : 'Placeholder'
-                                  }
-                                />
-                              )}
-                            </Grid.Column>
-                            <Grid.Column width={10}>
-                              <a href={product['@id']}>
-                                <strong>{product.title}</strong>
-                              </a>
-                              <p>{product.description}</p>
-                            </Grid.Column>
-                          </Grid>
-                        </li>
-                      ))}
-                    </ul>
-                  </AnimateHeight>
-                </Accordion.Content>
-              </Accordion>
-            )}
-            {content?.datasets?.length > 0 && (
-              <Accordion fluid styled>
-                <Accordion.Title
-                  as={'h2'}
-                  onClick={() => handleDatasetClick({ datasetindex: 0 })}
-                  className={'accordion-title align-arrow-right'}
-                >
-                  {activeDatasetIndex.includes(0) ? (
-                    <Icon
-                      name={iconName(content, titleIcons.opened)}
-                      size="24px"
-                    />
-                  ) : (
-                    <Icon
-                      name={iconName(content, titleIcons.closed)}
-                      size="24px"
-                    />
-                  )}
-                  <span>Related datasets</span>
-                </Accordion.Title>
-                <Accordion.Content active={activeDatasetIndex.includes(0)}>
-                  <AnimateHeight animateOpacity duration={500} height={'auto'}>
-                    <ul>
-                      {content.datasets.map((dataset, key) => (
-                        <li key={key}>
-                          <Grid columns={2}>
-                            <Grid.Column width={2}>
-                              {dataset.image_field && (
-                                <img
-                                  src={
-                                    dataset.image_field
-                                      ? `${dataset['@id']}/@@images/image`
-                                      : 'https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg'
-                                  }
-                                  alt={
-                                    dataset.image_field
-                                      ? dataset.image?.filename
-                                      : 'Placeholder'
-                                  }
-                                />
-                              )}
-                            </Grid.Column>
-                            <Grid.Column width={10}>
-                              <a href={dataset['@id']}>
-                                <strong>{dataset.title}</strong>
-                              </a>
-                              <p>{dataset.description}</p>
-                            </Grid.Column>
-                          </Grid>
-                        </li>
-                      ))}
-                    </ul>
-                  </AnimateHeight>
-                </Accordion.Content>
-              </Accordion>
-            )}
           </div>
         </>
+      )}
+      {files.length > 0 && (
+        <CclListingCards
+          variation="file"
+          items={files}
+          linkHref={`${files['@id']}/@@download/file`}
+        />
+      )}
+      {content?.products?.length > 0 && (
+        <Accordion fluid styled>
+          <Accordion.Title
+            as={'h2'}
+            onClick={() => handleClick({ index: 0 })}
+            className={'accordion-title align-arrow-right'}
+          >
+            {activeIndex.includes(0) ? (
+              <Icon name={iconName(content, titleIcons.opened)} size="24px" />
+            ) : (
+              <Icon name={iconName(content, titleIcons.closed)} size="24px" />
+            )}
+            <span>Related products</span>
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex.includes(0)}>
+            <AnimateHeight animateOpacity duration={500} height={'auto'}>
+              <ul>
+                {content.products.map((product, key) => (
+                  <li key={key}>
+                    <Grid columns={2}>
+                      <Grid.Column width={2}>
+                        {product.image_field && (
+                          <img
+                            src={
+                              product.image_field
+                                ? `${product['@id']}/@@images/image`
+                                : 'https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg'
+                            }
+                            alt={
+                              product.image_field
+                                ? product.image?.filename
+                                : 'Placeholder'
+                            }
+                          />
+                        )}
+                      </Grid.Column>
+                      <Grid.Column width={10}>
+                        <a href={product['@id']}>
+                          <strong>{product.title}</strong>
+                        </a>
+                        <p>{product.description}</p>
+                      </Grid.Column>
+                    </Grid>
+                  </li>
+                ))}
+              </ul>
+            </AnimateHeight>
+          </Accordion.Content>
+        </Accordion>
+      )}
+      {content?.datasets?.length > 0 && (
+        <Accordion fluid styled>
+          <Accordion.Title
+            as={'h2'}
+            onClick={() => handleDatasetClick({ datasetindex: 0 })}
+            className={'accordion-title align-arrow-right'}
+          >
+            {activeDatasetIndex.includes(0) ? (
+              <Icon name={iconName(content, titleIcons.opened)} size="24px" />
+            ) : (
+              <Icon name={iconName(content, titleIcons.closed)} size="24px" />
+            )}
+            <span>Related datasets</span>
+          </Accordion.Title>
+          <Accordion.Content active={activeDatasetIndex.includes(0)}>
+            <AnimateHeight animateOpacity duration={500} height={'auto'}>
+              <ul>
+                {content.datasets.map((dataset, key) => (
+                  <li key={key}>
+                    <Grid columns={2}>
+                      <Grid.Column width={2}>
+                        {dataset.image_field && (
+                          <img
+                            src={
+                              dataset.image_field
+                                ? `${dataset['@id']}/@@images/image`
+                                : 'https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg'
+                            }
+                            alt={
+                              dataset.image_field
+                                ? dataset.image?.filename
+                                : 'Placeholder'
+                            }
+                          />
+                        )}
+                      </Grid.Column>
+                      <Grid.Column width={10}>
+                        <a href={dataset['@id']}>
+                          <strong>{dataset.title}</strong>
+                        </a>
+                        <p>{dataset.description}</p>
+                      </Grid.Column>
+                    </Grid>
+                  </li>
+                ))}
+              </ul>
+            </AnimateHeight>
+          </Accordion.Content>
+        </Accordion>
       )}
     </div>
   );
