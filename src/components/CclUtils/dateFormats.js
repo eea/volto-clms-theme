b2d814331bd2a4f7bb6d7a3312e41d3a0fa52ddf
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-08-05 09:18:51 +0200

    new date handler that respects user's timezone

--- src/components/CclUtils/dateFormats.js
+++ src/components/CclUtils/dateFormats.js
@@ -1,4 +1,14 @@
-export const cclDateTimeFormat = (date) => {
+import { parseDateTime } from '@plone/volto/helpers';
+import moment from 'moment';
+import { useSelector } from 'react-redux';
+
+const getInternalValue = (value) => {
+  const lang = useSelector((state) => state.intl.locale);
+  moment.locale(lang);
+  return parseDateTime(lang, value, undefined, moment);
+};
+
+export const oldCclDateTimeFormat = (date) => {
   const dateObj = new Date(date);
   const day = ('0' + dateObj.getDate()).slice(-2);
   const month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
@@ -8,10 +18,20 @@ export const cclDateTimeFormat = (date) => {
   return `${day}.${month}.${year} ${hours}.${minutes}`;
 };
 
-export const cclDateFormat = (date) => {
+export const cclDateTimeFormat = (date) => {
+  const internal = getInternalValue(date);
+  return internal.format('DD.MM.YYYY hh.mm A');
+};
+
+export const oldCclDateFormat = (date) => {
   const dateObj = new Date(date);
   const day = dateObj.getDate();
   const month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
   const year = dateObj.getFullYear();
   return `${day}.${month}.${year}`;
+};
+
+export const cclDateFormat = (date) => {
+  const internal = getInternalValue(date);
+  return internal.format('DD.MM.YYYY');
 };
--- src/components/CclUtils/index.js
+++ src/components/CclUtils/index.js
@@ -2,4 +2,9 @@ import StringToHTML from './StringToHTML';
 
 export { StringToHTML };
 
-export { cclDateFormat, cclDateTimeFormat } from './dateFormats';
+export {
+  oldCclDateFormat,
+  oldCclDateTimeFormat,
+  cclDateFormat,
+  cclDateTimeFormat,
+} from './dateFormats';
