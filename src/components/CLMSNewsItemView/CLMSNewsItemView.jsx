8d3699b98e7aafb4d97ecae4679b2c6e25b6e4bb
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-08-31 15:07:53 +0200

    Revert "refactor(news events and datasets): remove related datasets and product accordions from news and events and replaced image scale used in datasets"
    This reverts commit 6ec497eba91bc7345e281a9996e77df2420175e3.

--- src/components/CLMSNewsItemView/CLMSNewsItemView.jsx
+++ src/components/CLMSNewsItemView/CLMSNewsItemView.jsx
@@ -5,13 +5,46 @@ import RenderBlocks from '@plone/volto/components/theme/View/RenderBlocks';
 import { cclDateFormat } from '@eeacms/volto-clms-theme/components/CclUtils';
 import { LightGalleryListing } from '@eeacms/volto-clms-theme/components/CLMSMeetingView/CclLightGallery';
 import CclListingCards from '@eeacms/volto-clms-theme/components/Blocks/CustomTemplates/VoltoListingBlock/CclListingCards';
+import config from '@plone/volto/registry';
+import { Icon } from '@plone/volto/components';
+import AnimateHeight from 'react-animate-height';
+import { Accordion } from 'semantic-ui-react';
+import { CLMSRelatedItems } from '../CLMSRelatedItems';
 
 const CLMSNewsItemView = (props) => {
   const { content } = props;
   const files = content.items
     ? content.items.filter((item) => item['@type'] === 'File')
     : [];
+  const [activeIndex, setActiveIndex] = React.useState([99]);
 
+  const handleClick = ({ index }) => {
+    const newIndex =
+      activeIndex.indexOf(index) === -1
+        ? [...activeIndex, index]
+        : activeIndex.filter((item) => item !== index);
+
+    setActiveIndex(newIndex);
+  };
+
+  const [activeDatasetIndex, setActiveDatasetIndex] = React.useState([99]);
+
+  const handleDatasetClick = ({ datasetindex }) => {
+    const newDatasetIndex =
+      activeDatasetIndex.indexOf(datasetindex) === -1
+        ? [...activeDatasetIndex, datasetindex]
+        : activeDatasetIndex.filter((item) => item !== datasetindex);
+
+    setActiveDatasetIndex(newDatasetIndex);
+  };
+
+  const titleIcons = config.blocks?.blocksConfig?.accordion?.titleIcons;
+
+  function iconName(iconData, iTitleIcons) {
+    return iconData?.right_arrows
+      ? iTitleIcons.rightPosition
+      : iTitleIcons.leftPosition;
+  }
   return (
     <div className="ccl-container">
       {hasBlocksData(content) && content.blocks_layout?.items?.length > 4 ? (
@@ -51,6 +84,48 @@ const CLMSNewsItemView = (props) => {
           items={files}
           linkHref={`${files['@id']}/@@download/file`}
         />
+      )}
+      {content?.products?.length > 0 && (
+        <Accordion fluid styled>
+          <Accordion.Title
+            as={'h2'}
+            onClick={() => handleClick({ index: 0 })}
+            className={'accordion-title align-arrow-right'}
+          >
+            {activeIndex.includes(0) ? (
+              <Icon name={iconName(content, titleIcons.opened)} size="24px" />
+            ) : (
+              <Icon name={iconName(content, titleIcons.closed)} size="24px" />
+            )}
+            <span>Related products</span>
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex.includes(0)}>
+            <AnimateHeight animateOpacity duration={500} height={'auto'}>
+              <CLMSRelatedItems items={content.products} />
+            </AnimateHeight>
+          </Accordion.Content>
+        </Accordion>
+      )}
+      {content?.datasets?.length > 0 && (
+        <Accordion fluid styled>
+          <Accordion.Title
+            as={'h2'}
+            onClick={() => handleDatasetClick({ datasetindex: 0 })}
+            className={'accordion-title align-arrow-right'}
+          >
+            {activeDatasetIndex.includes(0) ? (
+              <Icon name={iconName(content, titleIcons.opened)} size="24px" />
+            ) : (
+              <Icon name={iconName(content, titleIcons.closed)} size="24px" />
+            )}
+            <span>Related datasets</span>
+          </Accordion.Title>
+          <Accordion.Content active={activeDatasetIndex.includes(0)}>
+            <AnimateHeight animateOpacity duration={500} height={'auto'}>
+              <CLMSRelatedItems items={content.datasets} />
+            </AnimateHeight>
+          </Accordion.Content>
+        </Accordion>
       )}
     </div>
   );
