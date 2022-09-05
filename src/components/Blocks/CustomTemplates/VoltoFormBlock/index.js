33a222a38d74c95e8c706fce69a0334cf3c4f4e6
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-09-05 16:04:11 +0200

    Revert "Revert "new version of volto-form-block and modifications to add links to checkbox description""
    This reverts commit 369478c74ee083d9e2225fefe098b833ded6e66d.

---
+++ src/components/Blocks/CustomTemplates/VoltoFormBlock/index.js
@@ -0,0 +1,11 @@
+import CheckboxSchemaExtender from './CheckboxSchemaExtender';
+import customIdFieldSchema from './customIdFieldSchema';
+import CheckboxHtmlWidget from './CheckboxHtmlWidget';
+import VocabularyWidget from './VocabularyWidget';
+
+export {
+  CheckboxSchemaExtender,
+  customIdFieldSchema,
+  CheckboxHtmlWidget,
+  VocabularyWidget,
+};
