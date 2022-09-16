a6b8d27fdfdafbab599c7e8e60f42e0b45d7b5b5
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-09-16 09:08:07 +0200

    feat: use slate link editor with internal/external link editor and advanced options

--- src/index.js
+++ src/index.js
@@ -47,6 +47,9 @@ import CookieBanner from 'volto-cookie-banner/CookieBannerContainer';
 import CLMSLoginView from './components/CLMSLoginView/CLMSLogin';
 // import Login from '@plone/volto/components/Login';
 
+// custom link plugin for slate link inserter
+import installEditor from 'volto-slate/editor/plugins/Link';
+
 const applyConfig = (config) => {
   config.views = {
     ...config.views,
@@ -257,6 +260,9 @@ const applyConfig = (config) => {
     ...config.addonReducers,
     ...reducers,
   };
+
+  config = installEditor(config);
+
   return config;
 };
 export default applyConfig;
