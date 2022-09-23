3870362deaf735c35400404bfb39f84ac670177f
joewdavies <jwd101@gmail.com>
2022-09-23 11:46:27 +0200

    ESlint fix

--- src/customizations/volto/components/theme/Navigation/Navigation.jsx
+++ src/customizations/volto/components/theme/Navigation/Navigation.jsx
@@ -3,143 +3,142 @@
  * @module components/theme/Navigation/Navigation
  */
 
- import React, { Component } from 'react';
+import React, { Component } from 'react';
 
- import { NavLink } from 'react-router-dom';
- import PropTypes from 'prop-types';
- import { compose } from 'redux';
- import config from '@plone/volto/registry';
- import { connect } from 'react-redux';
- import { getBaseUrl } from '@plone/volto/helpers';
- import { getNavigation } from '@plone/volto/actions';
- import { injectIntl } from 'react-intl';
- 
- /**
-  * Navigation container class.
-  * @class Navigation
-  * @extends Component
-  */
- class Navigation extends Component {
-   /**
-    * Property types.
-    * @property {Object} propTypes Property types.
-    * @static
-    */
-   static propTypes = {
-     getNavigation: PropTypes.func.isRequired,
-     pathname: PropTypes.string.isRequired,
-     items: PropTypes.arrayOf(
-       PropTypes.shape({
-         title: PropTypes.string,
-         url: PropTypes.string,
-       }),
-     ).isRequired,
-     lang: PropTypes.string.isRequired,
-   };
- 
-   /**
-    * Constructor
-    * @method constructor
-    * @param {Object} props Component properties
-    * @constructs Navigation
-    */
-   constructor(props) {
-     super(props);
-     this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
-     this.closeMobileMenu = this.closeMobileMenu.bind(this);
-     this.state = {
-       isMobileMenuOpen: false,
-     };
-   }
- 
-   /**
-    * Component will mount
-    * @method componentWillMount
-    * @returns {undefined}
-    */
-   UNSAFE_componentWillMount() {
-     this.props.getNavigation(
-       getBaseUrl(this.props.pathname),
-       config.settings.navDepth,
-     );
-   }
- 
-   /**
-    * Component will receive props
-    * @method componentWillReceiveProps
-    * @param {Object} nextProps Next properties
-    * @returns {undefined}
-    */
-   UNSAFE_componentWillReceiveProps(nextProps) {
-     if (nextProps.pathname !== this.props.pathname) {
-       this.props.getNavigation(
-         getBaseUrl(nextProps.pathname),
-         config.settings.navDepth,
-       );
-     }
-   }
- 
-   /**
-    * Toggle mobile menu's open state
-    * @method toggleMobileMenu
-    * @returns {undefined}
-    */
-   toggleMobileMenu() {
-     this.setState({ isMobileMenuOpen: !this.state.isMobileMenuOpen });
-   }
- 
-   /**
-    * Close mobile menu
-    * @method closeMobileMenu
-    * @returns {undefined}
-    */
-   closeMobileMenu() {
-     this.setState({ isMobileMenuOpen: false });
-     this.props.setHeaderState({
-       mobileMenuOpen: false,
-     });
-   }
- 
-   /**
-    * Render method.
-    * @method render
-    * @returns {string} Markup for the component.
-    */
-   render() {
-     const { lang } = this.props;
- 
-     return (
-       <ul className="ccl-header-main-menu">
-         {this?.props?.items?.map((item) => (
-           <li key={item.url}>
-             <NavLink
-               to={item.url === '' ? '/' : item.url}
-               key={item.url}
-               activeClassName="active"
-               exact={
-                 config.settings.isMultilingual
-                   ? item.url === `/${lang}`
-                   : item.url === ''
-               }
-               onClick={this.closeMobileMenu}
-             >
-               {item.title}
-             </NavLink>
-           </li>
-         ))}
-       </ul>
-     );
-   }
- }
- 
- export default compose(
-   injectIntl,
-   connect(
-     (state) => ({
-       items: state.navigation.items,
-       lang: state.intl.locale,
-     }),
-     { getNavigation },
-   ),
- )(Navigation);
- 
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import config from '@plone/volto/registry';
+import { connect } from 'react-redux';
+import { getBaseUrl } from '@plone/volto/helpers';
+import { getNavigation } from '@plone/volto/actions';
+import { injectIntl } from 'react-intl';
+
+/**
+ * Navigation container class.
+ * @class Navigation
+ * @extends Component
+ */
+class Navigation extends Component {
+  /**
+   * Property types.
+   * @property {Object} propTypes Property types.
+   * @static
+   */
+  static propTypes = {
+    getNavigation: PropTypes.func.isRequired,
+    pathname: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        url: PropTypes.string,
+      }),
+    ).isRequired,
+    lang: PropTypes.string.isRequired,
+  };
+
+  /**
+   * Constructor
+   * @method constructor
+   * @param {Object} props Component properties
+   * @constructs Navigation
+   */
+  constructor(props) {
+    super(props);
+    this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
+    this.closeMobileMenu = this.closeMobileMenu.bind(this);
+    this.state = {
+      isMobileMenuOpen: false,
+    };
+  }
+
+  /**
+   * Component will mount
+   * @method componentWillMount
+   * @returns {undefined}
+   */
+  UNSAFE_componentWillMount() {
+    this.props.getNavigation(
+      getBaseUrl(this.props.pathname),
+      config.settings.navDepth,
+    );
+  }
+
+  /**
+   * Component will receive props
+   * @method componentWillReceiveProps
+   * @param {Object} nextProps Next properties
+   * @returns {undefined}
+   */
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.pathname !== this.props.pathname) {
+      this.props.getNavigation(
+        getBaseUrl(nextProps.pathname),
+        config.settings.navDepth,
+      );
+    }
+  }
+
+  /**
+   * Toggle mobile menu's open state
+   * @method toggleMobileMenu
+   * @returns {undefined}
+   */
+  toggleMobileMenu() {
+    this.setState({ isMobileMenuOpen: !this.state.isMobileMenuOpen });
+  }
+
+  /**
+   * Close mobile menu
+   * @method closeMobileMenu
+   * @returns {undefined}
+   */
+  closeMobileMenu() {
+    this.setState({ isMobileMenuOpen: false });
+    this.props.setHeaderState({
+      mobileMenuOpen: false,
+    });
+  }
+
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    const { lang } = this.props;
+
+    return (
+      <ul className="ccl-header-main-menu">
+        {this?.props?.items?.map((item) => (
+          <li key={item.url}>
+            <NavLink
+              to={item.url === '' ? '/' : item.url}
+              key={item.url}
+              activeClassName="active"
+              exact={
+                config.settings.isMultilingual
+                  ? item.url === `/${lang}`
+                  : item.url === ''
+              }
+              onClick={this.closeMobileMenu}
+            >
+              {item.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default compose(
+  injectIntl,
+  connect(
+    (state) => ({
+      items: state.navigation.items,
+      lang: state.intl.locale,
+    }),
+    { getNavigation },
+  ),
+)(Navigation);
