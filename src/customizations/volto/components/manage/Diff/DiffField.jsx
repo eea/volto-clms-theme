ba527ba3bbedbcf44a70ef68ee641d5bc97d50f3
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-12-21 14:12:23 +0100

    fix: add import/no-resolver ignore

--- src/customizations/volto/components/manage/Diff/DiffField.jsx
+++ src/customizations/volto/components/manage/Diff/DiffField.jsx
@@ -11,6 +11,7 @@ import { Grid } from 'semantic-ui-react';
 import ReactDOMServer from 'react-dom/server';
 import { Provider } from 'react-intl-redux';
 import { createBrowserHistory } from 'history';
+// eslint-disable-next-line import/no-unresolved
 import { ConnectedRouter } from 'connected-react-router';
 import { useSelector } from 'react-redux';
 
