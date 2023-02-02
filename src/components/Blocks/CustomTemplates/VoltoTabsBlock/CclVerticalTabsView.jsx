a1fede23fa0f76a02c4f4b13ab5bf57f1985550d
ionlizarazu <ilizarazu@codesyntax.com>
2023-02-02 10:13:36 +0100

    fix: extract TabsComponent and PanelsComponent from CclVerticalTabsView to avoid unneeded re-renderings

--- src/components/Blocks/CustomTemplates/VoltoTabsBlock/CclVerticalTabsView.jsx
+++ src/components/Blocks/CustomTemplates/VoltoTabsBlock/CclVerticalTabsView.jsx
@@ -12,103 +12,103 @@ import './fontawesome';
 
 import cx from 'classnames';
 
-const CclVerticalTabsView = (props) => {
+const handleClick = (e, tab, activeTab, setActiveTab) => {
+  if (activeTab !== tab) {
+    setActiveTab(tab);
+  }
+};
+
+function isSpan(subTab, nextSubTab) {
+  return subTab === false && nextSubTab !== false;
+}
+
+const TabsComponent = (props) => {
   const {
-    metadata = {},
     tabsList = [],
     ExtraComponent = () => {
       return '';
     },
+    activeTab = null,
+    tabs = {},
+    setActiveTab,
   } = props;
-
-  const handleClick = (e, tab, activeTab, setActiveTab) => {
-    if (activeTab !== tab) {
-      setActiveTab(tab);
-    }
-  };
-
-  function isSpan(subTab, nextSubTab) {
-    return subTab === false && nextSubTab !== false;
-  }
-
-  const PanelsComponent = () => {
-    const { activeTab = null, tabs = {} } = props;
-    return (
-      <div className="right-content cont-w-75">
+  return (
+    <div className="left-content cont-w-25">
+      {<ExtraComponent />}
+      <nav className="left-menu">
         {tabsList.map((tab, index) => {
           const title = tabs[tab].title;
+          const subTab = tabs[tab]?.subTab?.subtab || false;
+          const tabIndex = index + 1;
+          const nextSubTab = tabs[tabsList[tabIndex]]?.subTab?.subtab || false;
+          const defaultTitle = `Tab ${tabIndex}`;
           const tabHash = `tab=${slugify(title)}`;
           return (
-            <Route key={index} to={'#' + tabHash}>
-              <div
-                className={cx('panel', tab === activeTab && 'panel-selected')}
-                role="tabpanel"
-                aria-hidden="false"
-              >
-                <RenderBlocks
-                  {...props}
-                  metadata={metadata}
-                  content={tabs[tab]}
-                />
-              </div>
-            </Route>
+            <div
+              key={index}
+              id={tabHash}
+              className={cx(
+                'card',
+                tab === activeTab && 'active',
+                subTab && 'subcard',
+              )}
+            >
+              {isSpan(subTab, nextSubTab) ? (
+                <span>{title || defaultTitle}</span>
+              ) : (
+                <NavLink
+                  to={'#' + tabHash}
+                  className="collapsed"
+                  onClick={(e) => {
+                    handleClick(e, tab, activeTab, setActiveTab);
+                  }}
+                  onKeyDown={(e) => {
+                    handleClick(e, tab, activeTab, setActiveTab);
+                  }}
+                >
+                  {title || defaultTitle}
+                </NavLink>
+              )}
+            </div>
           );
         })}
-      </div>
-    );
-  };
-  const TabsComponent = () => {
-    return (
-      <div className="left-content cont-w-25">
-        {<ExtraComponent />}
-        <nav className="left-menu">
-          {tabsList.map((tab, index) => {
-            const { activeTab = null, tabs = {}, setActiveTab } = props;
-            const title = tabs[tab].title;
-            const subTab = tabs[tab]?.subTab?.subtab || false;
-            const tabIndex = index + 1;
-            const nextSubTab =
-              tabs[tabsList[tabIndex]]?.subTab?.subtab || false;
-            const defaultTitle = `Tab ${tabIndex}`;
-            const tabHash = `tab=${slugify(title)}`;
-            return (
-              <div
-                key={index}
-                id={tabHash}
-                className={cx(
-                  'card',
-                  tab === activeTab && 'active',
-                  subTab && 'subcard',
-                )}
-              >
-                {isSpan(subTab, nextSubTab) ? (
-                  <span>{title || defaultTitle}</span>
-                ) : (
-                  <NavLink
-                    to={'#' + tabHash}
-                    className="collapsed"
-                    onClick={(e) => {
-                      handleClick(e, tab, activeTab, setActiveTab);
-                    }}
-                    onKeyDown={(e) => {
-                      handleClick(e, tab, activeTab, setActiveTab);
-                    }}
-                  >
-                    {title || defaultTitle}
-                  </NavLink>
-                )}
-              </div>
-            );
-          })}
-        </nav>
-      </div>
-    );
-  };
+      </nav>
+    </div>
+  );
+};
 
+const PanelsComponent = (props) => {
+  const { metadata = {}, tabsList = [], activeTab = null, tabs = {} } = props;
+  return (
+    <div className="right-content cont-w-75">
+      {tabsList.map((tab, index) => {
+        const title = tabs[tab].title;
+        const tabHash = `tab=${slugify(title)}`;
+        return (
+          <Route key={index} to={'#' + tabHash}>
+            <div
+              className={cx('panel', tab === activeTab && 'panel-selected')}
+              role="tabpanel"
+              aria-hidden="false"
+            >
+              <RenderBlocks
+                {...props}
+                metadata={metadata}
+                content={tabs[tab]}
+              />
+            </div>
+          </Route>
+        );
+      })}
+    </div>
+  );
+};
+
+const CclVerticalTabsView = (props) => {
   return (
     <div className="ccl-container ccl-container-flex tab-container">
-      <TabsComponent />
-      <PanelsComponent />
+      <TabsComponent {...props} />
+      <PanelsComponent {...props} />
     </div>
   );
 };
