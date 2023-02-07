6fab93fcc4720f5c9731a3c7ae27829a5c3b439f
Unai <uetxaburu@codesyntax.eus>
2023-02-07 10:18:17 +0100

    new util and size shown at techniccal libraries inside datasets

--- src/components/Blocks/utils.js
+++ src/components/Blocks/utils.js
@@ -34,3 +34,12 @@ export const slugify = (string) => {
         .replace(/[^\w]+/g, '')
     : '';
 };
+
+export function formatFileSize(bytes, decimalPoint) {
+  if (bytes === 0) return '0 Bytes';
+  var k = 1000,
+    dm = decimalPoint || 2,
+    sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
