3f08fd7b013ab6704b57c25f2267a1953f20712d
Unai <uetxaburu@codesyntax.eus>
2023-02-27 12:13:34 +0100

    volto 16 header

--- src/customizations/volto/components/theme/Header/Header.jsx
+++ src/customizations/volto/components/theme/Header/Header.jsx
@@ -1,94 +1,26 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable no-undef */
 /**
  * Header component.
  * @module components/theme/Header/Header
  */
 
-import '@eeacms/volto-clms-theme/../theme/clms/css/header.css';
-
-import { FormattedMessage, injectIntl } from 'react-intl';
-import { Logo, Navigation, SearchWidget } from '@plone/volto/components';
-import React, { Component, useEffect } from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
-
-import { BodyClass } from '@plone/volto/helpers';
-// IMPORT isnt nedded until translations are created
-// import CclLanguageSelector from '@eeacms/volto-clms-theme/components/CclLanguageSelector/CclLanguageSelector';
-import CclLoginModal from '@eeacms/volto-clms-theme/components/CclLoginModal/CclLoginModal';
-import CclTopMainMenu from '@eeacms/volto-clms-theme/components/CclTopMainMenu/CclTopMainMenu';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Container, Segment } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { compose } from 'redux';
-import { getCartItems } from '@eeacms/volto-clms-utils/actions';
-import { getUser } from '@plone/volto/actions';
-import jwtDecode from 'jwt-decode';
+import { connect } from 'react-redux';
 
-const CartIconCounter = (props) => {
-  const cart = useSelector((state) => state.cart_items.items);
-  const intl = useSelector((state) => state.intl);
-  const user_id = useSelector((state) => state.users.user.id);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getCartItems(user_id));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user_id]);
-  return (
-    cart && (
-      <Link to={`/${intl.locale}/cart`} className="header-login-link">
-        <FontAwesomeIcon
-          icon={['fas', 'shopping-cart']}
-          style={{ marginRight: '0.25rem', maxWidth: '1.5rem' }}
-        />
-        <strong>{cart?.length}</strong>
-      </Link>
-    )
-  );
-};
-
-const HeaderDropdown = ({ user }) => {
-  const intl = useSelector((state) => state.intl);
-  return (
-    <>
-      <span>
-        <FontAwesomeIcon
-          icon={['fas', 'user']}
-          style={{ marginRight: '0.5rem' }}
-        />
-        {user?.fullname || user?.id || ''}
-        <span className="ccl-icon-chevron-thin-down"></span>
-      </span>
-      <ul>
-        <li>
-          <Link to={`/${intl.locale}/profile`} className="header-login-link">
-            My settings
-          </Link>
-        </li>
-        <li>
-          <Link
-            to={`/${intl.locale}/cart-downloads`}
-            className="header-login-link"
-          >
-            Downloads
-          </Link>
-        </li>
-        <li>
-          <Link to="/logout" className="header-login-link">
-            <FormattedMessage id="logout" defaultMessage="Logout" />
-          </Link>
-        </li>
-      </ul>
-    </>
-  );
-};
+import {
+  Anontools,
+  LanguageSelector,
+  Logo,
+  Navigation,
+  SearchWidget,
+} from '@plone/volto/components';
 
 /**
  * Header component class.
  * @class Header
  * @extends Component
  */
-
 class Header extends Component {
   /**
    * Property types.
@@ -98,27 +30,7 @@ class Header extends Component {
   static propTypes = {
     token: PropTypes.string,
     pathname: PropTypes.string.isRequired,
-    user: PropTypes.shape({
-      fullname: PropTypes.string,
-      email: PropTypes.string,
-      home_page: PropTypes.string,
-      location: PropTypes.string,
-      roles: PropTypes.array,
-    }).isRequired,
-    getUser: PropTypes.func.isRequired,
   };
-
-  componentDidMount() {
-    this.props.getUser(this.props.token);
-  }
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (nextProps.token !== this.props.token) {
-      this.props.getUser(nextProps.token);
-    }
-    if (nextProps.user.id !== this.props.user.id) {
-      this.props.getCartItems(this.props?.user?.id);
-    }
-  }
 
   /**
    * Default properties.
@@ -129,15 +41,6 @@ class Header extends Component {
     token: null,
   };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      mobileMenuOpen: false,
-      mobileSearchBoxOpen: false,
-    };
-  }
-
   /**
    * Render method.
    * @method render
@@ -145,158 +48,33 @@ class Header extends Component {
    */
   render() {
     return (
-      <div>
-        <header className="ccl-header">
-          {/* Body class depending on sections */}
-          <BodyClass className="ccl-style ccl-color_land" />
-
-          <div className="ccl-header-tools">
-            <div className="ccl-container">
-              <div
-                className="ccl-main-menu-collapse-button"
-                onClick={() =>
-                  this.setState({
-                    mobileMenuOpen: !this.state.mobileMenuOpen,
-                  })
-                }
-                onKeyDown={() =>
-                  this.setState({
-                    mobileMenuOpen: !this.state.mobileMenuOpen,
-                  })
-                }
-                tabIndex="0"
-                role="button"
-              >
-                <span
-                  className={
-                    this.state.mobileMenuOpen
-                      ? 'ccl-icon-close'
-                      : 'ccl-icon-menu'
-                  }
-                ></span>
+      <Segment basic className="header-wrapper" role="banner">
+        <Container>
+          <div className="header">
+            <div className="logo-nav-wrapper">
+              <div className="logo">
+                <Logo />
               </div>
-              <div
-                className="ccl-search-collapse-button"
-                onClick={() =>
-                  this.setState({
-                    mobileSearchBoxOpen: !this.state.mobileSearchBoxOpen,
-                  })
-                }
-                onKeyDown={() =>
-                  this.setState({
-                    mobileSearchBoxOpen: !this.state.mobileSearchBoxOpen,
-                  })
-                }
-                tabIndex="0"
-                role="button"
-              >
-                <span className="ccl-icon-zoom"></span>
-              </div>
-
-              <div className="ccl-header-tools-container">
-                <ul className="ccl-header-menu-tools">
-                  <CclTopMainMenu></CclTopMainMenu>
-                  <li className="header-vertical-line">
-                    <div>|</div>
-                  </li>
-                  {(this.props.user?.id && (
-                    <>
-                      <li className="header-dropdown">
-                        <HeaderDropdown user={this.props.user} />
-                      </li>
-                      <li>
-                        <CartIconCounter />
-                      </li>
-                    </>
-                  )) || (
-                    <li>
-                      <CclLoginModal />
-                    </li>
-                  )}
-                  <li className="header-vertical-line">
-                    <div>|</div>
-                  </li>
-                </ul>
-                <div
-                  className={
-                    this.state.mobileSearchBoxOpen
-                      ? 'ccl-header-search-show'
-                      : 'ccl-header-search-hidden'
-                  }
-                >
-                  <SearchWidget
-                    pathname={this.props.pathname}
-                    setHeaderState={(p) => {
-                      this.setState(p);
-                    }}
-                  />
+              <Navigation pathname={this.props.pathname} />
+            </div>
+            <div className="tools-search-wrapper">
+              <LanguageSelector />
+              {!this.props.token && (
+                <div className="tools">
+                  <Anontools />
                 </div>
-                {/* Language selector wont be shown until translations are completed */}
-                {/* <CclLanguageSelector /> */}
+              )}
+              <div className="search">
+                <SearchWidget />
               </div>
             </div>
           </div>
-          <div className="ccl-header-nav ">
-            <div className="ccl-container">
-              <Logo />
-              <nav
-                className={
-                  this.state.mobileMenuOpen
-                    ? 'ccl-main-menu ccl-collapsible-open'
-                    : 'ccl-main-menu'
-                }
-              >
-                <Navigation
-                  pathname={this.props.pathname}
-                  setHeaderState={(p) => {
-                    this.setState(p);
-                  }}
-                />
-                <ul className="ccl-header-menu-tools ccl-collapsible-toolmenu">
-                  <CclTopMainMenu></CclTopMainMenu>
-                  <li className="header-vertical-line">
-                    <div>|</div>
-                  </li>
-                  {(this.props.user.id && (
-                    <>
-                      <li className="header-dropdown">
-                        <HeaderDropdown user={this.props.user} />
-                      </li>
-                      <li>
-                        <CartIconCounter />
-                      </li>
-                    </>
-                  )) || (
-                    <li>
-                      <CclLoginModal />
-                    </li>
-                  )}
-                  <li className="header-vertical-line">
-                    <div>|</div>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-          <hr />
-        </header>
-      </div>
+        </Container>
+      </Segment>
     );
   }
 }
 
-export default compose(
-  injectIntl,
-  connect(
-    (state) => ({
-      locale: state.intl.locale,
-      cart: state.cart_items.items,
-      user: state.users.user,
-      token: state.userSession.token
-        ? jwtDecode(state.userSession.token).sub
-        : '',
-      rawtoken: state.userSession.token,
-    }),
-    { getUser, getCartItems },
-  ),
-)(Header);
+export default connect((state) => ({
+  token: state.userSession.token,
+}))(Header);
