20c51b310fd9b35d51c1361f604d96a01bc561a1
Unai <uetxaburu@codesyntax.eus>
2023-02-28 09:06:54 +0100

    volto 16 searchWidget

--- src/customizations/volto/components/theme/SearchWidget/SearchWidget.jsx
+++ src/customizations/volto/components/theme/SearchWidget/SearchWidget.jsx
@@ -4,12 +4,14 @@
  */
 
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { Form, Input } from 'semantic-ui-react';
 import { compose } from 'redux';
-import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { defineMessages, injectIntl } from 'react-intl';
-import { withRouter } from 'react-router-dom';
+
+import { Icon } from '@plone/volto/components';
+import zoomSVG from '@plone/volto/icons/zoom.svg';
 
 const messages = defineMessages({
   search: {
@@ -34,7 +36,7 @@ class SearchWidget extends Component {
    * @static
    */
   static propTypes = {
-    pathname: PropTypes.string.isRequired,
+    pathname: PropTypes.string,
   };
 
   /**
@@ -46,12 +48,9 @@ class SearchWidget extends Component {
   constructor(props) {
     super(props);
     this.onChangeText = this.onChangeText.bind(this);
-    this.onChangeSection = this.onChangeSection.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.state = {
-      lang: 'en',
       text: '',
-      section: false,
     };
   }
 
@@ -69,29 +68,23 @@ class SearchWidget extends Component {
   }
 
   /**
-   * On change section
-   * @method onChangeSection
-   * @param {object} event Event object.
-   * @param {bool} checked Section checked.
-   * @returns {undefined}
-   */
-  onChangeSection(event, { checked }) {
-    this.setState({
-      section: checked,
-    });
-  }
-
-  /**
    * Submit handler
    * @method onSubmit
    * @param {event} event Event object.
    * @returns {undefined}
    */
   onSubmit(event) {
-    const section = this.state.section ? `&path=${this.props.pathname}` : '';
+    const path =
+      this.props.pathname?.length > 0
+        ? `&path=${encodeURIComponent(this.props.pathname)}`
+        : '';
     this.props.history.push(
-      `/${this.props.locale}/global-search?SearchableText=${this.state.text}${section}`,
+      `/search?SearchableText=${encodeURIComponent(this.state.text)}${path}`,
     );
+    // reset input value
+    this.setState({
+      text: '',
+    });
     event.preventDefault();
   }
 
@@ -102,67 +95,25 @@ class SearchWidget extends Component {
    */
   render() {
     return (
-      <Form
-        className="ccl-header-search"
-        action="/global-search"
-        onMouseOut={(e) => {
-          if (e.relatedTarget) {
-            if (
-              !e.relatedTarget.offsetParent.classList.contains(
-                'ccl-header-search',
-              )
-            ) {
-              this.props.setHeaderState({ mobileSearchBoxOpen: false });
-            }
-          } else {
-            this.props.setHeaderState({ mobileSearchBoxOpen: false });
-          }
-        }}
-        onBlur={(e) => {
-          if (e.relatedTarget) {
-            if (
-              !e.relatedTarget.offsetParent.classList.contains(
-                'ccl-header-search',
-              )
-            ) {
-              this.props.setHeaderState({ mobileSearchBoxOpen: false });
-            }
-          } else {
-            this.props.setHeaderState({ mobileSearchBoxOpen: false });
-          }
-        }}
-      >
-        <Input
-          aria-label={this.props.intl.formatMessage(messages.search)}
-          onChange={this.onChangeText}
-          name="SearchableText"
-          value={this.state.text}
-          autoComplete="off"
-          placeholder={this.props.intl.formatMessage(messages.searchSite)}
-          title={this.props.intl.formatMessage(messages.search)}
-        />
-        <button
-          type="submit"
-          aria-label={this.props.intl.formatMessage(messages.search)}
-          onClick={this.onSubmit}
-        >
-          <span
-            className="ccl-icon-zoom"
-            role="button"
-            onClick={this.onSubmit}
-            onKeyDown={this.onSubmit}
-            tabIndex={0}
-          ></span>
-        </button>
+      <Form action="/search" onSubmit={this.onSubmit}>
+        <Form.Field className="searchbox">
+          <Input
+            aria-label={this.props.intl.formatMessage(messages.search)}
+            onChange={this.onChangeText}
+            name="SearchableText"
+            value={this.state.text}
+            transparent
+            autoComplete="off"
+            placeholder={this.props.intl.formatMessage(messages.searchSite)}
+            title={this.props.intl.formatMessage(messages.search)}
+          />
+          <button aria-label={this.props.intl.formatMessage(messages.search)}>
+            <Icon name={zoomSVG} size="18px" />
+          </button>
+        </Form.Field>
       </Form>
     );
   }
 }
 
-export default compose(
-  withRouter,
-  injectIntl,
-  connect((state) => ({
-    locale: state.intl.locale,
-  })),
-)(SearchWidget);
+export default compose(withRouter, injectIntl)(SearchWidget);
