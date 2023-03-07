dad046d5977d85694b5d3f1ad2125026bea95f12
ionlizarazu <ilizarazu@codesyntax.com>
2023-03-07 12:36:28 +0100

    ContextNavigationComponent import

--- src/components/Blocks/CclContextNavigationBlock/CclContextNavigationBlockView.jsx
+++ src/components/Blocks/CclContextNavigationBlock/CclContextNavigationBlockView.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContextNavigationComponent from '@plone/volto/components';
+import { ContextNavigationComponent } from '@plone/volto/components/theme/Navigation/ContextNavigation';
 
 const CclContextNavigationBlockView = (props) => {
   return <ContextNavigationComponent pathname={props.pathname} />;
