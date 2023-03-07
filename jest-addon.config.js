4bf039ea312de58fcb02600f248359914ae7291f
Unai <uetxaburu@codesyntax.eus>
2023-03-07 08:06:23 +0100

    jest addon config

--- jest-addon.config.js
+++ jest-addon.config.js
@@ -12,6 +12,8 @@ module.exports = {
     '@plone/volto-quanta/(.*)$': '<rootDir>/src/addons/volto-quanta/src/$1',
     '@eeacms/(.*?)/(.*)$': '<rootDir>/src/addons/$1/src/$2',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '@plone/volto-slate':
+      '<rootDir>/node_modules/@plone/volto/packages/volto-slate/src',
     '~/(.*)$': '<rootDir>/src/$1',
     'load-volto-addons':
       '<rootDir>/node_modules/@plone/volto/jest-addons-loader.js',
