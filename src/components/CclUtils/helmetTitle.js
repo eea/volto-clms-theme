efea8a668756a25e1fc7ada7d2b8b15cb308f1da
ionlizarazu <ilizarazu@codesyntax.com>
2023-03-23 10:08:09 +0100

    helmetTitle util

---
+++ src/components/CclUtils/helmetTitle.js
@@ -0,0 +1,8 @@
+export const helmetTitle = (title_tag_content, content) => {
+  let nav_root_title = content?.['@components']?.navroot?.title;
+  const result_title =
+    nav_root_title === title_tag_content
+      ? title_tag_content
+      : title_tag_content + ' — ' + nav_root_title;
+  return result_title.replace(/\u00AD/g, '');
+};
