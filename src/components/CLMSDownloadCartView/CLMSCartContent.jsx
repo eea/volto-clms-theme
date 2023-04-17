d42a563e1d08d49999199ea0af341c44140851c4
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-04-17 13:24:46 +0200

    fix: clms-2008 show bounding box coordinates

--- src/components/CLMSDownloadCartView/CLMSCartContent.jsx
+++ src/components/CLMSDownloadCartView/CLMSCartContent.jsx
@@ -184,7 +184,16 @@ const CLMSCartContent = (props) => {
     const { item } = areaProps;
     switch (item.area?.type) {
       case 'polygon':
-        return 'Bounding Box';
+        return (
+          <>
+            <span>Bounding Box</span>
+            <br />
+            <span>
+              {'('} {item.area.value.map((item) => item.toFixed(3)).join(', ')}
+              {')'}
+            </span>
+          </>
+        );
       case 'nuts':
         return 'NUTS: ' + (item.area.valueName || item.area.value);
       case undefined:
