89611cd6fde1e86b53dbf3d7f0abb2477ef20389
Unai <uetxaburu@codesyntax.eus>
2023-05-22 12:42:53 +0200

    feat CLMS:2142: first try changing home white bg block to use srcSet

--- src/components/Blocks/CclHomeBgImageBlock/CclWhiteBgView.jsx
+++ src/components/Blocks/CclHomeBgImageBlock/CclWhiteBgView.jsx
@@ -8,12 +8,36 @@ const CclWhiteBgView = (props) => {
   return (
     <div
       className="home-map-container"
-      style={{
-        backgroundImage:
-          `url(${data?.image?.url}/@@images/image)` ||
-          'url(https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg)',
-      }}
+      // style={{
+      //   backgroundImage: `url(${data?.image?.url}/@@images/image)
+      //   image-set(${data?.image?.url}/@@images/image/huge 1600w,
+      //     ${data?.image?.url}/@@images/image/great 1200w,
+      //     ${data?.image?.url}/@@images/image/larger 1000w,
+      //     ${data?.image?.url}/@@images/image/large 800w,
+      //     ${data?.image?.url}/@@images/image/teaser 600W,
+      //     )`,
+      // }}
     >
+      <img
+        src={
+          `${data?.image?.url}/@@images/image/huge` ||
+          'https://eu-copernicus.github.io/copernicus-component-library/assets/images/image_placeholder.jpg'
+        }
+        srcSet={`${data?.image?.url}/@@images/image/huge 2000w,
+          ${data?.image?.url}/@@images/image/huge 1600w,
+          ${data?.image?.url}/@@images/image/great 1200w,
+          ${data?.image?.url}/@@images/image/larger 1000w,
+          ${data?.image?.url}/@@images/image/large 800w,
+          ${data?.image?.url}/@@images/image/teaser 600W,
+          `}
+        sizes="(max-width: 767px) 540px,
+                               (min-width: 768px) and (max-width: 991px) 720px,
+                               (min-width: 992px) and (max-width: 1199px) 960px,
+                               (min-width: 1199px) and (max-width: 1399px) 1140px,
+                               (min-width: 1399px) and (max-width: 1920px) 1580px,
+                               (min-width: 2000) 1920px"
+        alt={data.image.alt}
+      />
       {isEditMode && <CclHomeImageEditor {...props} />}
       <div className="ccl-container">
         <div className="home-map-banner">
