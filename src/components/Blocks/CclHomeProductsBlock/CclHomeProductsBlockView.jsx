96469217a90a531b3c686ae8f394c4a9f52e38ce
Unai <uetxaburu@codesyntax.eus>
2023-11-28 09:22:20 +0100

    new branch for cache error fixes

--- src/components/Blocks/CclHomeProductsBlock/CclHomeProductsBlockView.jsx
+++ src/components/Blocks/CclHomeProductsBlock/CclHomeProductsBlockView.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { isUrl } from '@plone/volto/helpers';
+import { HashLink as Link } from 'react-router-hash-link';
+import { flattenToAppURL } from '@plone/volto/helpers/Url/Url';
 
 const CclHomeProductsBlockView = (props) => {
   const { data } = props;
@@ -9,42 +11,53 @@ const CclHomeProductsBlockView = (props) => {
   return (
     <div className="home-products-container">
       {products?.map((product, index) => (
-        <div
+        <Link
           key={index}
-          role="button"
-          className={
-            product.productIcon === 'iconless'
-              ? 'home-product home-product-main'
-              : 'home-product'
+          to={
+            isUrl(product?.linkSelector)
+              ? flattenToAppURL(product.linkSelector)
+              : isUrl('http://' + product?.linkSelector)
+              ? flattenToAppURL('http://' + product.linkSelector)
+              : flattenToAppURL(window.location + '#')
           }
-          tabIndex={0}
-          onClick={() => {
-            isUrl(product?.linkSelector)
-              ? window.location.assign(product.linkSelector)
-              : isUrl('http://' + product?.linkSelector)
-              ? window.location.assign('http://' + product.linkSelector)
-              : window.location.replace(window.location + '#');
-          }}
-          onKeyDown={() => {
-            isUrl(product?.linkSelector)
-              ? window.location.assign(product.linkSelector)
-              : isUrl('http://' + product?.linkSelector)
-              ? window.location.assign('http://' + product.linkSelector)
-              : window.location.replace(window.location + '#');
-          }}
-          id={product.productIcon === 'iconless' ? '' : product.productIcon}
         >
-          {product.productIcon === 'iconless' ? (
-            <h3>{product.title}</h3>
-          ) : (
-            <>
-              <div className="home-product-title">{product.title}</div>
-              <div className="home-product-description">
-                {product.description}
-              </div>
-            </>
-          )}
-        </div>
+          <div
+            // key={index}
+            // role="button"
+            className={
+              product.productIcon === 'iconless'
+                ? 'home-product home-product-main'
+                : 'home-product'
+            }
+            // tabIndex={0}
+            // onClick={() => {
+            //   isUrl(product?.linkSelector)
+            //     ? window.location.assign(product.linkSelector)
+            //     : isUrl('http://' + product?.linkSelector)
+            //     ? window.location.assign('http://' + product.linkSelector)
+            //     : window.location.replace(window.location + '#');
+            // }}
+            // onKeyDown={() => {
+            //   isUrl(product?.linkSelector)
+            //     ? window.location.assign(product.linkSelector)
+            //     : isUrl('http://' + product?.linkSelector)
+            //     ? window.location.assign('http://' + product.linkSelector)
+            //     : window.location.replace(window.location + '#');
+            // }}
+            id={product.productIcon === 'iconless' ? '' : product.productIcon}
+          >
+            {product.productIcon === 'iconless' ? (
+              <h3>{product.title}</h3>
+            ) : (
+              <>
+                <div className="home-product-title">{product.title}</div>
+                <div className="home-product-description">
+                  {product.description}
+                </div>
+              </>
+            )}
+          </div>
+        </Link>
       ))}
     </div>
   );
