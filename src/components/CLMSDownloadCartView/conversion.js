962abacd4b9deeef18eaa2568843d42005a1c5f8
Ion Lizarazu <ilizarazu@codesyntax.com>
2023-11-29 15:40:39 +0100

    fix: CLMS-2827 - download cart format options texts

--- src/components/CLMSDownloadCartView/conversion.js
+++ src/components/CLMSDownloadCartView/conversion.js
@@ -1,3 +1,14 @@
+const DATASET_FORMATS_NAMES = {
+  Shapefile: 'Shapefile (SHP)',
+  GDB: 'File Geodatabase (FGDB)',
+  GPKG: 'GeoPackage (GPKG)',
+  Geojson: 'GeoJSON',
+  Geotiff: 'GeoTIFF',
+  Netcdf: 'Network Common Data Form (NetCDF)',
+  GML: 'Geography Markup Language (GML)',
+  WFS: 'WFS',
+};
+
 export const getAvailableConversion = (conversionTable, defaultValue) => {
   if (conversionTable && Object.keys(conversionTable).length > 0) {
     if (
@@ -12,10 +23,13 @@ export const getAvailableConversion = (conversionTable, defaultValue) => {
         return conversionTable[defaultValue][key];
       })
       .map((item) => {
+        const text = DATASET_FORMATS_NAMES[item]
+          ? DATASET_FORMATS_NAMES[item]
+          : item;
         return {
           key: item,
           value: item,
-          text: item,
+          text: text,
         };
       });
   } else {
