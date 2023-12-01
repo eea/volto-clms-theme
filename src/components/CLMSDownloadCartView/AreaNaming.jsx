30c4edef70c5af090591afdb4b9c75465b07179c
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-12-01 12:37:47 +0100

    Revert "cart modal created"
    This reverts commit 27cda3b95436e42483caf12c1b8d7008541e4e71.

--- src/components/CLMSDownloadCartView/AreaNaming.jsx
+++ src/components/CLMSDownloadCartView/AreaNaming.jsx
@@ -1,33 +1,11 @@
 import { Label } from 'semantic-ui-react';
-import CclModal from '@eeacms/volto-clms-theme/components/CclModal/CclModal';
-import LatLonImg from '@eeacms/volto-clms-theme/../theme/clms/img/lat_lon.png';
-
 export const AreaNaming = (areaProps) => {
   const { item } = areaProps;
   switch (item?.area?.type) {
     case 'polygon':
       return (
         <>
-          <CclModal
-            draggable={true}
-            trigger={<span>Bounding Box</span>}
-            size={'medium'}
-          >
-            <div className="image-modal">
-              <img src={LatLonImg} alt={'Lat Lon World'} />
-              <span className="cart-bounding-boxes">
-                <span className="cart-bounding-box-row floating-modal-row">
-                  <Label>{`N: ${item?.area.value[1].toFixed(1)}º`}</Label>&nbsp;
-                  <Label>{`E: ${item?.area.value[2].toFixed(1)}º`}</Label>&nbsp;
-                  {/* </span>
-            <span className="cart-bounding-box-row"> */}
-                  <Label>{`S: ${item?.area.value[3].toFixed(1)}º`}</Label>&nbsp;
-                  <Label>{`W: ${item?.area.value[0].toFixed(1)}º`}</Label>
-                </span>
-              </span>
-            </div>
-          </CclModal>
-
+          <span>Bounding Box</span>
           <br />
           <span className="cart-bounding-boxes">
             <span className="cart-bounding-box-row">
