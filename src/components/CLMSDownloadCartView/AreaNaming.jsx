0bad1b04ddfa7639b0a5125176f95c5e27c78759
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-12-01 14:23:23 +0100

    Revert "Revert "zoom in cart modal""
    This reverts commit 99cc45b91c52d0dae180d616c4a4b8d5a10b733a.

--- src/components/CLMSDownloadCartView/AreaNaming.jsx
+++ src/components/CLMSDownloadCartView/AreaNaming.jsx
@@ -1,6 +1,9 @@
+import React from 'react';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import { Label } from 'semantic-ui-react';
-import CclModal from '@eeacms/volto-clms-theme/components/CclModal/CclModal';
+
 import LatLonImg from '@eeacms/volto-clms-theme/../theme/clms/img/lat_lon.png';
+import CclModal from '@eeacms/volto-clms-theme/components/CclModal/CclModal';
 
 export const AreaNaming = (areaProps) => {
   const { item } = areaProps;
@@ -14,7 +17,35 @@ export const AreaNaming = (areaProps) => {
             size={'medium'}
           >
             <div className="image-modal">
-              <img src={LatLonImg} alt={'Lat Lon World'} />
+              <TransformWrapper>
+                {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                  <React.Fragment>
+                    <div className="tools">
+                      <button
+                        className="ccl-button ccl-button--default"
+                        onClick={() => zoomIn()}
+                      >
+                        Zoom in
+                      </button>
+                      <button
+                        className="ccl-button ccl-button--default"
+                        onClick={() => zoomOut()}
+                      >
+                        Zoom out
+                      </button>
+                      <button
+                        className="ccl-button ccl-button--default"
+                        onClick={() => resetTransform()}
+                      >
+                        Reset zoom
+                      </button>
+                    </div>
+                    <TransformComponent>
+                      <img src={LatLonImg} alt={'Lat Lon World'} />
+                    </TransformComponent>
+                  </React.Fragment>
+                )}
+              </TransformWrapper>
               <span className="modal-cart-bounding-boxes">
                 <span className="cart-bounding-box-row">
                   <Label>{`N: ${item?.area.value[1].toFixed(1)}º`}</Label>&nbsp;
