2a0edb936adffdfd20a61fbef69f86fa85954eb2
Unai <uetxaburu@codesyntax.eus>
2024-01-24 08:27:55 +0100

    fix: CLMS-3026

--- src/components/CclCard/CclCard.jsx
+++ src/components/CclCard/CclCard.jsx
@@ -218,6 +218,7 @@ function CclCard(props) {
     'file',
     'downloadFile',
     'fileWithoutDates',
+    'cardWithBgImage',
   ];
   const wrapperClass =
     'card-' +
@@ -399,6 +400,83 @@ function CclCard(props) {
                   <p className="card-event-description">{card?.description}</p>
                 </div>
               </>
+            )}
+            {type === 'cardWithBgImage' && (
+              <div className="home-map-container">
+                {card?.image?.url && (
+                  <HomeBgImg url={card?.image?.url} alt={card.image.alt} />
+                )}
+                {isEditMode && <CclHomeImageEditor {...props} />}
+                <div className="ccl-container">
+                  <div className="home-map-banner">
+                    {data.title && <h1>{data?.title}</h1>}
+                    {data.subtitle && <h2>{data?.subtitle}</h2>}
+                    {data.description && <h3>{data?.description} </h3>}
+                    {data?.hasButton === true && (
+                      <CclButton
+                        url={
+                          data?.download &&
+                          data?.href?.[0]?.['@type'] === 'File'
+                            ? data?.href?.[0]?.['@id'] + '/@@download/file'
+                            : data?.href?.[0]?.['@id']
+                        }
+                        disabled={data?.disabled}
+                        download={
+                          data?.download ||
+                          data?.href?.[0]?.['@type'] === 'File'
+                        }
+                        target={
+                          data?.target ||
+                          (data?.download &&
+                            data?.href[0]['@type'] === 'File' &&
+                            '_blank')
+                        }
+                        mode={data?.style}
+                      >
+                        {data?.buttonTitle}
+                      </CclButton>
+                    )}
+                  </div>
+                  {data?.dataset_title ||
+                    data?.dataset_subtitle ||
+                    (data?.dataset_description && (
+                      <div className="home-dataset-banner">
+                        {data.dataset_title && <h1>{data?.dataset_title}</h1>}
+                        {data.dataset_subtitle && (
+                          <h2>{data?.dataset_subtitle}</h2>
+                        )}
+                        {data.dataset_description && (
+                          <h3>{data?.dataset_description} </h3>
+                        )}
+                        {data?.hasDatasetButton === true && (
+                          <CclButton
+                            url={
+                              data?.download &&
+                              data?.dataset_href?.[0]?.['@type'] === 'File'
+                                ? data?.dataset_href?.[0]?.['@id'] +
+                                  '/@@download/file'
+                                : data?.dataset_href?.[0]?.['@id']
+                            }
+                            disabled={data?.dataset_disabled}
+                            download={
+                              data?.download ||
+                              data?.dataset_href?.[0]?.['@type'] === 'File'
+                            }
+                            target={
+                              data?.target ||
+                              (data?.download &&
+                                data?.dataset_href[0]['@type'] === 'File' &&
+                                '_blank')
+                            }
+                            mode={data?.dataset_style}
+                          >
+                            {data?.dataset_buttonTitle}
+                          </CclButton>
+                        )}
+                      </div>
+                    ))}
+                </div>
+              </div>
             )}
           </>
         ) : (
