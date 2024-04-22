9959c4065c3d0e292a9baaa5916b11c6b7645b30
Ion Lizarazu <ilizarazu@codesyntax.com>
2024-04-22 12:44:36 +0200

    add a 3 Column Cards list (with dates) variation

--- src/components/CclCard/CclCard.jsx
+++ src/components/CclCard/CclCard.jsx
@@ -215,6 +215,7 @@ function CclCard(props) {
     'event',
     'block',
     'threeColumns',
+    'threeColumnsDates',
     'globalSearch',
     'file',
     'downloadFile',
@@ -233,13 +234,15 @@ function CclCard(props) {
       ? 'line card-line-no-description'
       : type === 'cardWithBgImage'
       ? 'home-map-banner'
+      : 'threeColumnsDates'
+      ? 'threeColumns'
       : type || 'line');
 
   return (
     <CardLink
       url={url}
       className={wrapperClass}
-      condition={type === 'block' || type === 'threeColumns'}
+      condition={['block', 'threeColumns', 'threeColumnsDates'].includes(type)}
     >
       <div
         tabIndex="0"
@@ -247,7 +250,8 @@ function CclCard(props) {
         onClick={() => onClickImage()}
         onKeyDown={() => onClickImage()}
         className={
-          !(type === 'block' || type === 'threeColumns') && wrapperClass
+          !['block', 'threeColumns', 'threeColumnsDates'].includes(type) &&
+          wrapperClass
         }
       >
         {conditional_types.includes(type) ? (
@@ -326,9 +330,13 @@ function CclCard(props) {
                 </DocCard>
               </>
             )}
-            {(type === 'block' || type === 'threeColumns') && (
+            {['block', 'threeColumns', 'threeColumnsDates'].includes(type) && (
               <>
-                <div className={`card-${type}-image`}>
+                <div
+                  className={`card-${
+                    type === 'threeColumnsDates' ? 'threeColumns' : type
+                  }-image`}
+                >
                   {isCustomCard && CclImageEditor ? (
                     CclImageEditor
                   ) : (
@@ -341,6 +349,11 @@ function CclCard(props) {
                 </div>
                 <div className="card-text">
                   <div className="card-title">{card?.title}</div>
+                  {type === 'threeColumnsDates' && (
+                    <div className="card-date">
+                      {cclDateFormat(card?.effective)}
+                    </div>
+                  )}
                   <div className="card-description">{card?.description}</div>
                   {children}
                 </div>
