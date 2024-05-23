141b173a1d4a1032f00a780181c3c6037015b5a4
Unai Etxaburu <uetxaburu@codesyntax.eus>
2024-05-23 16:01:45 +0200

    regex sanitazed

--- src/components/CclUtils/sanitizedHTML.js
+++ src/components/CclUtils/sanitizedHTML.js
@@ -2,10 +2,10 @@ export const sanitizedHTML = (strToSanitize) => {
   if (strToSanitize === undefined) {
     return '';
   }
-  const parser = new DOMParser();
-  let myHTML = parser.parseFromString(strToSanitize, 'text/html');
-  // let test = strToSanitize.replace(/(<([^>]+)>)/gi, '');
-  // console.log('myHTML', myHTML);
+  // const parser = new DOMParser();
+  // let myHTML = parser.parseFromString(strToSanitize, 'text/html');
+  let myHTML = strToSanitize.replace(/(<([^>]+)>)/gi, '');
+  console.log('myHTML', myHTML);
   // console.log('test', test);
-  return myHTML.body.textContent || '';
+  return myHTML || '';
 };
