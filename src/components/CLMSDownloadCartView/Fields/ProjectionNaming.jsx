e37590ab0069dcadbcb99308f5698e93d9274afc
Unai Bolivar <ujbolivar@gmail.com>
2024-07-18 12:45:34 +0200

    CLMS-271247 (task): When the selected format is NetCDF, the projection combo should have EPSG:4326 value

--- src/components/CLMSDownloadCartView/Fields/ProjectionNaming.jsx
+++ src/components/CLMSDownloadCartView/Fields/ProjectionNaming.jsx
@@ -20,7 +20,9 @@ const _ProjectionNaming = ({
   const setProjectionValue = (unique_id, value, cI) => {
     const new_cartItems = [...cI];
     const objIndex = cI.findIndex((obj) => obj.unique_id === unique_id);
-    if (new_cartItems[objIndex]) new_cartItems[objIndex].projection = value;
+    if (new_cartItems[objIndex]) {
+      new_cartItems[objIndex].projection = value;
+    }
     setCartItems([...new_cartItems]);
   };
   const utm = getUtm(item, utmProjections);
@@ -50,38 +52,49 @@ const _ProjectionNaming = ({
       projections_uid?.[item.dataset_uid].length > 0
     ) {
       setLoading(true);
-      setChoices(
-        projections_uid?.[item.dataset_uid]
-          ?.sort((a, b) => {
-            if (Number(a.split(':')[1]) > Number(b.split(':')[1])) {
-              return 1;
-            } else {
-              return -1;
-            }
-          })
-          .filter((p) => utm.includes(p) || baseSources.includes(p))
-          .map((p) => {
-            const re = new RegExp(p.split(':')[1]);
-            return {
-              key: p,
-              value: p,
-              default: re.test(item.original_projection) ? true : false,
-              text: re.test(item.original_projection)
-                ? `${p} (Source system of the dataset)`
-                : p,
-              className: re.test(item.original_projection)
-                ? 'original_projection'
-                : 'projection',
-            };
-          }),
+      const newBaseSources = ['EPSG:4326'];
+      const sortedProjections = projections_uid[item.dataset_uid].sort((a, b) =>
+        Number(a.split(':')[1]) > Number(b.split(':')[1]) ? 1 : -1,
       );
+
+      const filteredProjections =
+        item.format === 'Netcdf' ||
+        item.format.token === 'Netcdf' ||
+        item.format.title === 'Netcdf'
+          ? sortedProjections.filter(
+              (p) => utm.includes(p) || newBaseSources.includes(p),
+            )
+          : sortedProjections.filter(
+              (p) => utm.includes(p) || baseSources.includes(p),
+            );
+
+      const choices = filteredProjections.map((p) => {
+        const re = new RegExp(p.split(':')[1]);
+        return {
+          key: p,
+          value: p,
+          default: re.test(item.original_projection),
+          text: re.test(item.original_projection)
+            ? `${p} (Source system of the dataset)`
+            : p,
+          className: re.test(item.original_projection)
+            ? 'original_projection'
+            : 'projection',
+        };
+      });
+
+      setChoices(choices);
     } else {
       setLoading(true);
     }
     return () => {};
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [JSON.stringify(projections_uid?.[item.dataset_uid]), item.unique_id]);
+  }, [
+    JSON.stringify(projections_uid?.[item.dataset_uid]),
+    item.unique_id,
+    item.format,
+  ]);
 
   useEffect(() => {
     !item.projection && choices.length > 0 && choices.find((ch) => ch.default)
