b2f064ec64f7beaf313b13891f78d58c302c547f
GhitaB <ghitabizau27@gmail.com>
2024-08-22 11:22:49 +0300

    Refs #273992 - CclModal.test.jsx - Enzyme -> RTL.

--- src/components/CclModal/CclModal.test.js
+++ src/components/CclModal/CclModal.test.js
@@ -1,19 +1,10 @@
-import Enzyme, { mount } from 'enzyme';
-
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { render, screen, fireEvent } from '@testing-library/react';
 import CclModal from './CclModal';
-import React from 'react';
-import renderer from 'react-test-renderer';
-import configureStore from 'redux-mock-store';
 import { Provider } from 'react-intl-redux';
+import React from 'react';
+import configureStore from 'redux-mock-store';
 
-Enzyme.configure({ adapter: new Adapter() });
 const mockStore = configureStore();
-
-jest.mock('@plone/volto/helpers/Loadable/Loadable');
-beforeAll(async () => {
-  await require('@plone/volto/helpers/Loadable/Loadable').__setLoadables();
-});
 
 describe('CclModal', () => {
   const store = mockStore({
@@ -25,66 +16,54 @@ describe('CclModal', () => {
       message: 'You are not authorized to access this resource',
     },
   });
+
   it('Check external link', () => {
-    const component = renderer
-      .create(
-        <Provider store={store}>
-          <CclModal
-            trigger={
-              <div className="header-lang-icon">
-                <i className="ccl-icon-language"></i>
-                <span className="header-lang-code">EN</span>
-              </div>
-            }
-          >
-            <p>Hello test!</p>
-          </CclModal>
-        </Provider>,
-      )
-      .toJSON();
-    expect(component).toBeDefined();
+    const { asFragment } = render(
+      <Provider store={store}>
+        <CclModal
+          trigger={
+            <div className="header-lang-icon">
+              <i className="ccl-icon-language"></i>
+              <span className="header-lang-code">EN</span>
+            </div>
+          }
+        >
+          <p>Hello test!</p>
+        </CclModal>
+      </Provider>,
+    );
+    expect(asFragment()).toBeDefined();
   });
 
-  it('Check if clicks works', () => {
-    // const mockClicks = jest.fn();
-    // const wrapper = mount(
-    //   ,
-    // );
-
+  it('Check if clicks work', () => {
     const mockCallBack = jest.fn();
-    const component = mount(
-      <CclModal
-        trigger={
-          <a href="/login" className="header-login-link" onClick={mockCallBack}>
-            Login/Register
-          </a>
-        }
-        size="tiny"
-      >
-        <p>Hello test!</p>
-      </CclModal>,
+
+    render(
+      <Provider store={store}>
+        <CclModal
+          trigger={
+            <a
+              href="/login"
+              className="header-login-link"
+              onClick={mockCallBack}
+            >
+              Login/Register
+            </a>
+          }
+          size="tiny"
+        >
+          <p>Hello test!</p>
+        </CclModal>
+      </Provider>,
     );
-    // const spy = jest.spyOn(component.instance(), 'toggleSort');
-    const mobileLang = component.find('.header-login-link');
-    mobileLang.simulate('click');
 
-    const closeModal = component.find('.ccl-icon-close');
-    closeModal.simulate('click').simulate('keydown');
-    // component.find('.header-login-link').at(0).simulate('click');
-    expect(mockCallBack.mock.calls.length).toEqual(1);
-    // const mobileSel = wrapper.find('div.header-lang-icon');
-    // mobileSel.simulate('click');
+    const loginLink = screen.getByText('Login/Register');
+    fireEvent.click(loginLink);
 
-    // const mobileLang = wrapper.find('.language-link a');
-    // mobileLang.simulate('click');
+    const closeModalButton = screen.getByRole('button', { name: /close/i });
+    fireEvent.click(closeModalButton);
+    fireEvent.keyDown(closeModalButton, { key: 'Escape', code: 'Escape' });
 
-    // // mobileSel.simulate('click');
-
-    // const deskSel = wrapper.find('.header-lang-text span');
-    // deskSel.simulate('click');
-
-    // const deskLang = wrapper.find('.header-lang-text .language-link a');
-    // deskLang.simulate('click');
-    // expect(component).toBeDefined();
+    expect(mockCallBack).toHaveBeenCalledTimes(1);
   });
 });
