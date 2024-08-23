20984ccfe404038500ecdaf7866f2512d1358469
GhitaB <ghitabizau27@gmail.com>
2024-08-23 11:22:05 +0300

    Refs #273992 - Fix CartIconCounter.test.jsx.

--- src/components/CartIconCounter/CartIconCounter.test.jsx
+++ src/components/CartIconCounter/CartIconCounter.test.jsx
@@ -1,19 +1,12 @@
 import { onlyInLeft, CartIconCounter } from './CartIconCounter';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import renderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-intl-redux';
+import { MemoryRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
-Enzyme.configure({ adapter: new Adapter() });
 
-jest.mock('@plone/volto/helpers/Loadable/Loadable');
-beforeAll(async () => {
-  await require('@plone/volto/helpers/Loadable/Loadable').__setLoadables();
-});
+const mockStore = configureStore();
 
 describe('onlyInLeft', () => {
-  // Returns an array with items that are only in the left array, based on a compare function
   it('should return an array with items that are only in the left array', () => {
     const left = [
       { id: 1, name: 'Item 1' },
@@ -34,28 +27,7 @@ describe('onlyInLeft', () => {
       { id: 3, name: 'Item 3' },
     ]);
   });
-  // Should return an array with items that are only in the left array, based on a compare function
-  it('should return an array with items that are only in the left array', () => {
-    const left = [
-      { id: 1, name: 'Item 1' },
-      { id: 2, name: 'Item 2' },
-      { id: 3, name: 'Item 3' },
-    ];
-    const right = [
-      { id: 2, name: 'Item 2' },
-      { id: 4, name: 'Item 4' },
-    ];
-    const compareFunction = (leftValue, rightValue) =>
-      leftValue.id === rightValue.id;
 
-    const result = onlyInLeft(left, right, compareFunction);
-
-    expect(result).toEqual([
-      { id: 1, name: 'Item 1' },
-      { id: 3, name: 'Item 3' },
-    ]);
-  });
-  // should return an empty array if left array is empty
   it('should return an empty array when left array is empty', () => {
     const left = [];
     const right = [
@@ -70,7 +42,6 @@ describe('onlyInLeft', () => {
     expect(result).toEqual([]);
   });
 
-  // should return the left array if right array is empty
   it('should return the left array when the right array is empty', () => {
     const left = [
       { id: 1, name: 'Item 1' },
@@ -85,32 +56,9 @@ describe('onlyInLeft', () => {
 
     expect(result).toEqual(left);
   });
-  // should return an array with items that are only in the left array
-  it('should return an array with items that are only in the left array', () => {
-    const left = [
-      { id: 1, name: 'Item 1' },
-      { id: 2, name: 'Item 2' },
-      { id: 3, name: 'Item 3' },
-    ];
-    const right = [
-      { id: 2, name: 'Item 2' },
-      { id: 4, name: 'Item 4' },
-    ];
-    const compareFunction = (leftValue, rightValue) =>
-      leftValue.id === rightValue.id;
-
-    const result = onlyInLeft(left, right, compareFunction);
-
-    expect(result).toEqual([
-      { id: 1, name: 'Item 1' },
-      { id: 3, name: 'Item 3' },
-    ]);
-  });
 });
 
-const mockStore = configureStore();
 describe('CartIconCounter', () => {
-  // Cart icon displays number of items in cart
   it('should display the correct number of items in the cart', () => {
     const store = mockStore({
       cart_items: {
@@ -129,18 +77,15 @@ describe('CartIconCounter', () => {
         messages: {},
       },
     });
-    // Render the component
-    const component = renderer.create(
+
+    const { getByText } = render(
       <Provider store={store}>
         <MemoryRouter>
           <CartIconCounter />
         </MemoryRouter>
       </Provider>,
     );
-    // const component = mount(<CartIconCounter />);
 
-    // Expect the component toBeDefined
-    expect(component).toBeDefined();
-    expect(component.toJSON().children[1].children[0]).toEqual('3');
+    expect(getByText('3')).toBeInTheDocument();
   });
 });
