a1cb44f5ba60a6a9bb50572e0ae93e9001360ba2
GhitaB <ghitabizau27@gmail.com>
2025-05-20 15:07:40 +0300

    Refs #286059 - Technical Library search: Categories facet.

--- src/search/facets.js
+++ src/search/facets.js
@@ -1,10 +1,9 @@
 import globalSearchBaseConfig from '@eeacms/volto-globalsearch/config/global-search-base-config.js';
 
-// TODO clean
-export const copernicus_components = {
-  field: 'taxonomy_copernicus_components.keyword',
+export const library_categories = {
+  field: 'library_categories_facet.keyword',
   factory: 'MultiTermFacet',
-  label: 'Copernicus components',
+  label: 'Categories',
   showInFacetsList: true,
   filterType: 'any',
   isFilterable: false,
@@ -12,22 +11,6 @@ export const copernicus_components = {
   isMulti: true,
 };
 
-// TODO clean
-export const copernicus_services = {
-  field: 'copernicus_services.keyword',
-  factory: 'MultiTermFacet',
-  label: 'Copernicus services',
-  showInFacetsList: true,
-  filterType: 'any',
-  isFilterable: false,
-  show: 10000,
-  isMulti: true,
-};
-
-const facets = [
-  copernicus_components,
-  copernicus_services,
-  ...globalSearchBaseConfig.facets,
-];
+const facets = [library_categories, ...globalSearchBaseConfig.facets];
 
 export default facets;
