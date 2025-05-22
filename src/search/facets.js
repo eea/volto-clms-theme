b719964dfb2b01b7879cf0724431f144b6b6a937
GhitaB <ghitabizau27@gmail.com>
2025-05-22 16:24:07 +0300

    Refs #286059 - Technical Library search: Set All time as default instead of Last 5 years.

--- src/search/facets.js
+++ src/search/facets.js
@@ -13,4 +13,10 @@ export const library_categories = {
 
 const facets = [library_categories, ...globalSearchBaseConfig.facets];
 
+// Change default Last 5 years with All time
+const facet = facets.find((f) => f.field === 'issued.date');
+if (facet && facet.default && Array.isArray(facet.default.values)) {
+  facet.default.values[0] = 'All time';
+}
+
 export default facets;
